067e893437870b037b1ad57f97c52239f9eb3237
Loïc Knuchel <loic.knuchel@doctolib.com>
2021-09-18 13:34:54 +0200

    add experiment page

---
+++ src/Pages/Experiments.elm
@@ -0,0 +1,63 @@
+module Pages.Experiments exposing (Model, Msg, page)
+
+import Gen.Params.Experiments exposing (Params)
+import Page
+import Request
+import Shared
+import View exposing (View)
+
+
+page : Shared.Model -> Request.With Params -> Page.With Model Msg
+page shared req =
+    Page.element
+        { init = init
+        , update = update
+        , view = view
+        , subscriptions = subscriptions
+        }
+
+
+
+-- INIT
+
+
+type alias Model =
+    {}
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( {}, Cmd.none )
+
+
+
+-- UPDATE
+
+
+type Msg
+    = ReplaceMe
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        ReplaceMe ->
+            ( model, Cmd.none )
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+
+-- VIEW
+
+
+view : Model -> View Msg
+view model =
+    View.placeholder "Experiments"
