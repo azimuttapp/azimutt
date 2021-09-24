8af03ceacb5c1fb7ad4e1733902b5b3fe37c1aa8
Loïc Knuchel <loic.knuchel@doctolib.com>
2021-09-24 21:54:23 +0200

    generate pages

---
+++ src/Pages/Blog.elm
@@ -0,0 +1,64 @@
+module Pages.Blog exposing (Model, Msg, page)
+
+import Gen.Params.Blog exposing (Params)
+import Libs.Task exposing (send)
+import Page
+import Request
+import Shared
+import View exposing (View)
+
+
+page : Shared.Model -> Request.With Params -> Page.With Model Msg
+page _ _ =
+    Page.element
+        { init = init
+        , update = update
+        , view = view
+        , subscriptions = subscriptions
+        }
+
+
+
+-- INIT
+
+
+type alias Model =
+    {}
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( {}, send ReplaceMe )
+
+
+
+-- UPDATE
+
+
+type Msg
+    = ReplaceMe
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        ReplaceMe ->
+            ( model, Cmd.none )
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Sub.none
+
+
+
+-- VIEW
+
+
+view : Model -> View Msg
+view _ =
+    View.placeholder "Blog"
