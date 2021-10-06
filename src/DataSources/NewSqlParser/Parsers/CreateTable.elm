b91bb035d220025ec0c4b023eb75a4fc58cb628b
Loïc Knuchel <loic.knuchel@doctolib.com>
2021-10-06 20:40:06 +0200

    refacto

--- src/DataSources/NewSqlParser/Parsers/CreateTable.elm
+++ src/DataSources/NewSqlParser/Parsers/CreateTable.elm
@@ -1,6 +1,7 @@
-module DataSources.NewSqlParser.Parsers.CreateTable exposing (parseCreateTable)
+module DataSources.NewSqlParser.Parsers.CreateTable exposing (createTableParser)
 
-import DataSources.NewSqlParser.Utils.Types exposing (ParseError, ParsedTable, SqlStatement)
+import DataSources.NewSqlParser.Utils.Types exposing (ParseError, ParsedColumn, ParsedTable, SqlStatement)
+import Parser exposing ((|.), (|=), Parser, Trailing(..), chompIf, chompWhile, getChompedString, sequence, spaces, succeed, symbol)
 
 
 
@@ -11,6 +12,55 @@ import DataSources.NewSqlParser.Utils.Types exposing (ParseError, ParsedTable, S
 -- https://www.sqlite.org/lang_createtable.html
 
 
-parseCreateTable : SqlStatement -> Result (List ParseError) ParsedTable
-parseCreateTable statement =
-    Err []
+createTableParser : Parser ParsedTable
+createTableParser =
+    succeed (\tableName columns -> ParsedTable Nothing tableName columns)
+        |. symbol "CREATE TABLE"
+        |. spaces
+        |= tableNameParser
+        |. spaces
+        |= parseColumns
+
+
+tableNameParser : Parser String
+tableNameParser =
+    getChompedString <|
+        succeed ()
+            |. chompIf Char.isAlpha
+            |. chompWhile (\c -> c /= ' ' && c /= '(')
+
+
+parseColumns : Parser (List ParsedColumn)
+parseColumns =
+    sequence
+        { start = "("
+        , separator = ","
+        , end = ")"
+        , spaces = spaces
+        , item = parseColumn
+        , trailing = Forbidden -- demand a trailing semi-colon
+        }
+
+
+parseColumn : Parser ParsedColumn
+parseColumn =
+    succeed (\name kind -> ParsedColumn name kind True Nothing Nothing Nothing Nothing)
+        |= columnName
+        |. spaces
+        |= columnType
+
+
+columnName : Parser String
+columnName =
+    getChompedString <|
+        succeed ()
+            |. chompIf Char.isAlpha
+            |. chompWhile (\c -> c /= ' ' && c /= '(')
+
+
+columnType : Parser String
+columnType =
+    getChompedString <|
+        succeed ()
+            |. chompIf Char.isAlpha
+            |. chompWhile (\c -> c /= ' ' && c /= ',' && c /= '\n')
