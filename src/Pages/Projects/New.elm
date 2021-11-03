76447ebf4339a75b958796ed8ef6491b298b828b
Loïc Knuchel <loic.knuchel@doctolib.com>
2021-11-03 20:14:40 +0100

    bootstrap pages

---
+++ src/Pages/Projects/New.elm
@@ -0,0 +1,64 @@
+module Pages.Projects.New exposing (Model, Msg, page)
+
+import Gen.Params.Projects.New exposing (Params)
+import Libs.Task exposing (send)
+import Page
+import Request
+import Shared
+import View exposing (View)
+
+
+page : Shared.Model -> Request.With Params -> Page.With Model Msg
+page _ _ =
+    Page.element
+        { init = init
+        , update = update
+        , view = view
+        , subscriptions = subscriptions
+        }
+
+
+type alias Model =
+    {}
+
+
+type Msg
+    = ReplaceMe
+
+
+
+-- INIT
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( {}, send ReplaceMe )
+
+
+
+-- UPDATE
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        ReplaceMe ->
+            ( model, Cmd.none )
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+    Sub.none
+
+
+
+-- VIEW
+
+
+view : Model -> View Msg
+view _ =
+    View.placeholder "Projects.New"
