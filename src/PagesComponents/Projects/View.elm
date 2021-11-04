849c25adff80a63c8b411ac88eb1150225fdcca9
Loïc Knuchel <loic.knuchel@doctolib.com>
2021-11-04 12:36:20 +0100

    extract nav links

--- src/PagesComponents/Projects/View.elm
+++ src/PagesComponents/Projects/View.elm
@@ -1,13 +1,33 @@
 module PagesComponents.Projects.View exposing (viewProjects)
 
 import Components.Atoms.Icon as Icon
-import Css exposing (focus, hover)
+import Css exposing (Style, focus, hover)
 import Css.Global as Global
 import Html.Styled exposing (Html, a, button, div, h1, header, img, input, label, main_, nav, span, text)
 import Html.Styled.Attributes exposing (alt, class, css, for, href, id, name, placeholder, src, tabindex, type_)
 import Libs.Html.Styled.Attributes exposing (ariaControls, ariaCurrent, ariaExpanded, ariaHaspopup, ariaLabelledby, ariaOrientation, role)
+import Libs.Maybe as M
 import Tailwind.Breakpoints exposing (lg, sm)
 import Tailwind.Utilities exposing (absolute, bg_indigo_500, bg_indigo_600, bg_indigo_700, bg_opacity_75, bg_white, block, border, border_4, border_b, border_dashed, border_gray_200, border_indigo_300, border_indigo_400, border_indigo_700, border_none, border_opacity_25, border_t, border_transparent, border_white, flex, flex_1, flex_shrink_0, font_bold, font_medium, globalStyles, h_10, h_16, h_8, h_96, hidden, inline_flex, inset_y_0, items_center, justify_between, justify_center, justify_end, leading_5, left_0, max_w_7xl, max_w_lg, max_w_xs, min_h_full, ml_10, ml_3, ml_4, ml_6, ml_auto, mt_2, mt_3, mx_auto, neg_mt_32, origin_top_right, outline_none, p_1, p_2, pb_12, pb_3, pb_32, pl_10, pl_3, placeholder_gray_500, pointer_events_none, pr_3, pt_2, pt_4, px_0, px_2, px_3, px_4, px_5, px_6, px_8, py_1, py_10, py_2, py_6, relative, right_0, ring_1, ring_2, ring_black, ring_offset_2, ring_offset_indigo_600, ring_opacity_5, ring_white, rounded_full, rounded_lg, rounded_md, shadow, shadow_lg, space_x_4, space_y_1, sr_only, text_3xl, text_base, text_gray_400, text_gray_700, text_gray_900, text_indigo_200, text_indigo_300, text_sm, text_white, w_10, w_48, w_8, w_full)
+
+
+type alias Link =
+    { url : String, text : String }
+
+
+currentPage : Maybe String
+currentPage =
+    Just "Dashboard"
+
+
+navLinks : List Link
+navLinks =
+    [ { url = "#", text = "Dashboard" }
+    , { url = "#", text = "Team" }
+    , { url = "#", text = "Projects" }
+    , { url = "#", text = "Calendar" }
+    , { url = "#", text = "Reports" }
+    ]
 
 
 viewProjects : List (Html msg)
@@ -22,15 +42,7 @@ viewProjects =
                             [ div [ css [ flex_shrink_0 ] ]
                                 [ img [ css [ block, h_8, w_8 ], src "https://tailwindui.com/img/logos/workflow-mark-indigo-300.svg", alt "Workflow" ] []
                                 ]
-                            , div [ css [ hidden, lg [ block, ml_10 ] ] ]
-                                [ div [ css [ flex, space_x_4 ] ]
-                                    [ {- Current: "bg-indigo-700 text-white", Default: "text-white hover:bg-indigo-500 hover:bg-opacity-75" -} a [ href "#", css [ bg_indigo_700, text_white, rounded_md, py_2, px_3, text_sm, font_medium ], ariaCurrent "page" ] [ text "Dashboard" ]
-                                    , a [ href "#", css [ text_white, rounded_md, py_2, px_3, text_sm, font_medium, hover [ bg_indigo_500, bg_opacity_75 ] ] ] [ text "Team" ]
-                                    , a [ href "#", css [ text_white, rounded_md, py_2, px_3, text_sm, font_medium, hover [ bg_indigo_500, bg_opacity_75 ] ] ] [ text "Projects" ]
-                                    , a [ href "#", css [ text_white, rounded_md, py_2, px_3, text_sm, font_medium, hover [ bg_indigo_500, bg_opacity_75 ] ] ] [ text "Calendar" ]
-                                    , a [ href "#", css [ text_white, rounded_md, py_2, px_3, text_sm, font_medium, hover [ bg_indigo_500, bg_opacity_75 ] ] ] [ text "Reports" ]
-                                    ]
-                                ]
+                            , div [ css [ hidden, lg [ block, ml_10 ] ] ] [ viewNavLinks navLinks currentPage ]
                             ]
                         , div [ css [ flex_1, px_2, flex, justify_center, lg [ ml_6, justify_end ] ] ]
                             [ div [ css [ max_w_lg, w_full, lg [ max_w_xs ] ] ]
@@ -87,13 +99,7 @@ viewProjects =
                     ]
                 , {- Mobile menu, show/hide based on menu state. -}
                   div [ css [ lg [ hidden ] ], id "mobile-menu" ]
-                    [ div [ css [ px_2, pt_2, pb_3, space_y_1 ] ]
-                        [ {- Current: "bg-indigo-700 text-white", Default: "text-white hover:bg-indigo-500 hover:bg-opacity-75" -} a [ href "#", css [ bg_indigo_700, text_white, block, rounded_md, py_2, px_3, text_base, font_medium ], ariaCurrent "page" ] [ text "Dashboard" ]
-                        , a [ href "#", css [ text_white, block, rounded_md, py_2, px_3, text_base, font_medium, Css.hover [ bg_indigo_500, bg_opacity_75 ] ] ] [ text "Team" ]
-                        , a [ href "#", css [ text_white, block, rounded_md, py_2, px_3, text_base, font_medium, Css.hover [ bg_indigo_500, bg_opacity_75 ] ] ] [ text "Projects" ]
-                        , a [ href "#", css [ text_white, block, rounded_md, py_2, px_3, text_base, font_medium, Css.hover [ bg_indigo_500, bg_opacity_75 ] ] ] [ text "Calendar" ]
-                        , a [ href "#", css [ text_white, block, rounded_md, py_2, px_3, text_base, font_medium, Css.hover [ bg_indigo_500, bg_opacity_75 ] ] ] [ text "Reports" ]
-                        ]
+                    [ viewMobileNavLinks navLinks currentPage
                     , div [ css [ pt_4, pb_3, border_t, border_indigo_700 ] ]
                         [ div [ css [ px_5, flex, items_center ] ]
                             [ div [ css [ flex_shrink_0 ] ]
@@ -103,22 +109,22 @@ viewProjects =
                                 [ div [ css [ text_base, font_medium, text_white ] ] [ text "Tom Cook" ]
                                 , div [ css [ text_sm, font_medium, text_indigo_300 ] ] [ text "tom@example.com" ]
                                 ]
-                            , button [ type_ "button", css [ ml_auto, bg_indigo_600, flex_shrink_0, rounded_full, p_1, text_indigo_200, Css.focus [ outline_none, ring_2, ring_offset_2, ring_offset_indigo_600, ring_white ], Css.hover [ text_white ] ] ]
+                            , button [ type_ "button", css [ ml_auto, bg_indigo_600, flex_shrink_0, rounded_full, p_1, text_indigo_200, focus [ outline_none, ring_2, ring_offset_2, ring_offset_indigo_600, ring_white ], hover [ text_white ] ] ]
                                 [ span [ css [ sr_only ] ] [ text "View notifications" ]
                                 , Icon.bell 6 []
                                 ]
                             ]
                         , div [ css [ mt_3, px_2, space_y_1 ] ]
-                            [ a [ href "#", css [ block, rounded_md, py_2, px_3, text_base, font_medium, text_white, Css.hover [ bg_indigo_500, bg_opacity_75 ] ] ] [ text "Your Profile" ]
-                            , a [ href "#", css [ block, rounded_md, py_2, px_3, text_base, font_medium, text_white, Css.hover [ bg_indigo_500, bg_opacity_75 ] ] ] [ text "Settings" ]
-                            , a [ href "#", css [ block, rounded_md, py_2, px_3, text_base, font_medium, text_white, Css.hover [ bg_indigo_500, bg_opacity_75 ] ] ] [ text "Sign out" ]
+                            [ a [ href "#", css [ block, rounded_md, py_2, px_3, text_base, font_medium, text_white, hover [ bg_indigo_500, bg_opacity_75 ] ] ] [ text "Your Profile" ]
+                            , a [ href "#", css [ block, rounded_md, py_2, px_3, text_base, font_medium, text_white, hover [ bg_indigo_500, bg_opacity_75 ] ] ] [ text "Settings" ]
+                            , a [ href "#", css [ block, rounded_md, py_2, px_3, text_base, font_medium, text_white, hover [ bg_indigo_500, bg_opacity_75 ] ] ] [ text "Sign out" ]
                             ]
                         ]
                     ]
                 ]
             , header [ css [ py_10 ] ]
                 [ div [ css [ max_w_7xl, mx_auto, px_4, lg [ px_8 ], sm [ px_6 ] ] ]
-                    [ h1 [ css [ text_3xl, font_bold, text_white ] ] [ text "Dashboard" ]
+                    [ h1 [ css [ text_3xl, font_bold, text_white ] ] [ text "My projects" ]
                     ]
                 ]
             ]
@@ -134,3 +140,22 @@ viewProjects =
             ]
         ]
     ]
+
+
+viewNavLinks : List Link -> Maybe String -> Html msg
+viewNavLinks links current =
+    div [ css [ flex, space_x_4 ] ] (links |> List.map (viewLink [ text_sm ] current))
+
+
+viewMobileNavLinks : List Link -> Maybe String -> Html msg
+viewMobileNavLinks links current =
+    div [ css [ px_2, pt_2, pb_3, space_y_1 ] ] (links |> List.map (viewLink [ block, text_base ] current))
+
+
+viewLink : List Style -> Maybe String -> Link -> Html msg
+viewLink styles current link =
+    if current |> M.contains link.text then
+        a [ href link.url, css ([ text_white, rounded_md, py_2, px_3, font_medium, bg_indigo_700 ] ++ styles), ariaCurrent "page" ] [ text link.text ]
+
+    else
+        a [ href link.url, css ([ text_white, rounded_md, py_2, px_3, font_medium, hover [ bg_indigo_500, bg_opacity_75 ] ] ++ styles) ] [ text link.text ]
