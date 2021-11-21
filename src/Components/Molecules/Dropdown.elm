a87c1a5eb2163381927dc53c6ff11a1670d14012
Loïc Knuchel <loic.knuchel@doctolib.com>
2021-11-21 12:21:25 +0100

    add animation to dropdown

--- src/Components/Molecules/Dropdown.elm
+++ src/Components/Molecules/Dropdown.elm
@@ -1,7 +1,8 @@
-module Components.Molecules.Dropdown exposing (DocState, Model, SharedDocState, doc, initDocState, simple)
+module Components.Molecules.Dropdown exposing (DocState, Model, SharedDocState, doc, dropdown, initDocState)
 
 import Components.Atoms.Icon as Icon exposing (Icon(..))
 import Css
+import Dict exposing (Dict)
 import ElmBook exposing (Msg)
 import ElmBook.Actions as Actions
 import ElmBook.Chapter as Chapter
@@ -9,21 +10,30 @@ import ElmBook.ElmCSS exposing (Chapter)
 import Html.Styled exposing (Html, a, button, div, text)
 import Html.Styled.Attributes exposing (css, href, id, tabindex, type_)
 import Html.Styled.Events exposing (onClick)
-import Libs.Bool as B
+import Libs.Dict as D
 import Libs.Html.Styled.Attributes exposing (ariaExpanded, ariaHaspopup, ariaLabelledby, ariaOrientation, role)
 import Libs.Models.HtmlId exposing (HtmlId)
 import Tailwind.Utilities as Tw
 
 
 type alias Model =
-    { id : HtmlId }
+    { id : HtmlId, isOpen : Bool }
 
 
-simple : Model -> Bool -> (Model -> Html msg) -> (Model -> List (Html msg)) -> Html msg
-simple model isOpen elt content =
+dropdown : Model -> (Model -> Html msg) -> (Model -> List (Html msg)) -> Html msg
+dropdown model elt content =
+    let
+        dropdownMenu : List Css.Style
+        dropdownMenu =
+            if model.isOpen then
+                [ Tw.transition, Tw.ease_in, Tw.duration_75, Tw.opacity_100, Tw.transform, Tw.scale_100 ]
+
+            else
+                [ Tw.transition, Tw.ease_out, Tw.duration_100, Tw.opacity_0, Tw.transform, Tw.scale_95, Tw.pointer_events_none ]
+    in
     div [ css [ Tw.relative, Tw.inline_block, Tw.text_left ] ]
         [ elt model
-        , div [ role "menu", ariaOrientation "vertical", ariaLabelledby model.id, tabindex -1, css ([ Tw.origin_top_right, Tw.absolute, Tw.left_0, Tw.mt_2, Tw.w_56, Tw.rounded_md, Tw.shadow_lg, Tw.bg_white, Tw.ring_1, Tw.ring_black, Tw.ring_opacity_5, Css.focus [ Tw.outline_none ] ] ++ B.cond isOpen [] [ Tw.hidden ]) ]
+        , div [ role "menu", ariaOrientation "vertical", ariaLabelledby model.id, tabindex -1, css ([ Tw.origin_top_right, Tw.absolute, Tw.left_0, Tw.mt_2, Tw.w_56, Tw.rounded_md, Tw.shadow_lg, Tw.bg_white, Tw.ring_1, Tw.ring_black, Tw.ring_opacity_5, Css.focus [ Tw.outline_none ] ] ++ dropdownMenu) ]
             [ div [ role "none", css [ Tw.py_1 ] ] (content model)
             ]
         ]
@@ -38,29 +48,41 @@ type alias SharedDocState x =
 
 
 type alias DocState =
-    { simpleOpen : Bool }
+    { isOpen : Dict String Bool }
 
 
 initDocState : DocState
 initDocState =
-    { simpleOpen = False }
+    { isOpen = Dict.empty }
 
 
-update : (DocState -> DocState) -> Msg (SharedDocState x)
-update transform =
+updateDocState : (DocState -> DocState) -> Msg (SharedDocState x)
+updateDocState transform =
     Actions.updateState (\s -> { s | dropdownDocState = s.dropdownDocState |> transform })
+
+
+component : String -> (Bool -> (Bool -> Msg (SharedDocState x)) -> Html msg) -> ( String, SharedDocState x -> Html msg )
+component name buildComponent =
+    ( name
+    , \{ dropdownDocState } ->
+        buildComponent
+            (dropdownDocState.isOpen |> D.getOrElse name False)
+            (\isOpen -> updateDocState (\s -> { s | isOpen = s.isOpen |> Dict.insert name isOpen }))
+    )
 
 
 doc : Chapter (SharedDocState x)
 doc =
     Chapter.chapter "Dropdown"
         |> Chapter.renderStatefulComponentList
-            [ ( "simple"
-              , \{ dropdownDocState } ->
-                    simple { id = "menu-button" }
-                        dropdownDocState.simpleOpen
+            [ component "simple"
+                (\isOpen setIsOpen ->
+                    dropdown
+                        { id = "menu-button"
+                        , isOpen = isOpen
+                        }
                         (\model ->
-                            button [ type_ "button", id model.id, ariaExpanded True, ariaHaspopup True, onClick (update (\m -> { m | simpleOpen = not m.simpleOpen })), css [ Tw.inline_flex, Tw.justify_center, Tw.w_full, Tw.rounded_md, Tw.border, Tw.border_gray_300, Tw.shadow_sm, Tw.px_4, Tw.py_2, Tw.bg_white, Tw.text_sm, Tw.font_medium, Tw.text_gray_700, Css.focus [ Tw.outline_none, Tw.ring_2, Tw.ring_offset_2, Tw.ring_offset_gray_100, Tw.ring_indigo_500 ], Css.hover [ Tw.bg_gray_50 ] ] ]
+                            button [ type_ "button", id model.id, ariaExpanded True, ariaHaspopup True, onClick (setIsOpen (not isOpen)), css [ Tw.inline_flex, Tw.justify_center, Tw.w_full, Tw.rounded_md, Tw.border, Tw.border_gray_300, Tw.shadow_sm, Tw.px_4, Tw.py_2, Tw.bg_white, Tw.text_sm, Tw.font_medium, Tw.text_gray_700, Css.focus [ Tw.outline_none, Tw.ring_2, Tw.ring_offset_2, Tw.ring_offset_gray_100, Tw.ring_indigo_500 ], Css.hover [ Tw.bg_gray_50 ] ] ]
                                 [ text "Options"
                                 , {- Heroicon name: solid/chevron-down -} Icon.view ChevronDown []
                                 ]
@@ -71,5 +93,5 @@ doc =
                             , a [ href "#", css [ Tw.text_gray_700, Tw.block, Tw.px_4, Tw.py_2, Tw.text_sm, Css.hover [ Tw.bg_gray_100, Tw.text_gray_900 ] ], role "menuitem", tabindex -1, id "menu-item-2" ] [ text "License" ]
                             ]
                         )
-              )
+                )
             ]
--- src/Components/Molecules/Modal.elm
+++ src/Components/Molecules/Modal.elm
@@ -33,44 +33,44 @@ type alias ConfirmModel msg =
 
 
 confirm : ConfirmModel msg -> Bool -> Html msg
-confirm model =
+confirm model isOpen =
     modal
         { id = model.id
+        , isOpen = isOpen
         , onBackgroundClick = model.onCancel
-        , content =
-            [ div [ css [ Bp.sm [ Tw.flex, Tw.items_start ] ] ]
-                [ div [ css [ Tw.mx_auto, Tw.flex_shrink_0, Tw.flex, Tw.items_center, Tw.justify_center, Tw.h_12, Tw.w_12, Tw.rounded_full, TwColor.render Bg model.color L100, Bp.sm [ Tw.mx_0, Tw.h_10, Tw.w_10 ] ] ]
-                    [ Icon.view model.icon [ TwColor.render Text model.color L600 ]
-                    ]
-                , div [ css [ Tw.mt_3, Tw.text_center, Bp.sm [ Tw.mt_0, Tw.ml_4, Tw.text_left ] ] ]
-                    [ h3 [ css [ Tw.text_lg, Tw.leading_6, Tw.font_medium, Tw.text_gray_900 ], id model.id ]
-                        [ text model.title ]
-                    , div [ css [ Tw.mt_2 ] ]
-                        [ p [ css [ Tw.text_sm, Tw.text_gray_500 ] ] [ text model.description ]
-                        ]
-                    ]
+        }
+        [ div [ css [ Bp.sm [ Tw.flex, Tw.items_start ] ] ]
+            [ div [ css [ Tw.mx_auto, Tw.flex_shrink_0, Tw.flex, Tw.items_center, Tw.justify_center, Tw.h_12, Tw.w_12, Tw.rounded_full, TwColor.render Bg model.color L100, Bp.sm [ Tw.mx_0, Tw.h_10, Tw.w_10 ] ] ]
+                [ Icon.view model.icon [ TwColor.render Text model.color L600 ]
                 ]
-            , div [ css [ Tw.mt_5, Bp.sm [ Tw.mt_4, Tw.flex, Tw.flex_row_reverse ] ] ]
-                [ Button.primary3 model.color [ onClick model.onConfirm, css [ Tw.w_full, Tw.text_base, Bp.sm [ Tw.ml_3, Tw.w_auto, Tw.text_sm ] ] ] [ text model.confirm ]
-                , Button.white3 model.color [ onClick model.onCancel, css [ Tw.mt_3, Tw.w_full, Tw.text_base, Bp.sm [ Tw.mt_0, Tw.w_auto, Tw.text_sm ] ] ] [ text model.cancel ]
+            , div [ css [ Tw.mt_3, Tw.text_center, Bp.sm [ Tw.mt_0, Tw.ml_4, Tw.text_left ] ] ]
+                [ h3 [ css [ Tw.text_lg, Tw.leading_6, Tw.font_medium, Tw.text_gray_900 ], id model.id ]
+                    [ text model.title ]
+                , div [ css [ Tw.mt_2 ] ]
+                    [ p [ css [ Tw.text_sm, Tw.text_gray_500 ] ] [ text model.description ]
+                    ]
                 ]
             ]
-        }
+        , div [ css [ Tw.mt_5, Bp.sm [ Tw.mt_4, Tw.flex, Tw.flex_row_reverse ] ] ]
+            [ Button.primary3 model.color [ onClick model.onConfirm, css [ Tw.w_full, Tw.text_base, Bp.sm [ Tw.ml_3, Tw.w_auto, Tw.text_sm ] ] ] [ text model.confirm ]
+            , Button.white3 model.color [ onClick model.onCancel, css [ Tw.mt_3, Tw.w_full, Tw.text_base, Bp.sm [ Tw.mt_0, Tw.w_auto, Tw.text_sm ] ] ] [ text model.cancel ]
+            ]
+        ]
 
 
 type alias Model msg =
     { id : HtmlId
+    , isOpen : Bool
     , onBackgroundClick : msg
-    , content : List (Html msg)
     }
 
 
-modal : Model msg -> Bool -> Html msg
-modal model isOpen =
+modal : Model msg -> List (Html msg) -> Html msg
+modal model content =
     let
         modalContainer : List Css.Style
         modalContainer =
-            if isOpen then
+            if model.isOpen then
                 []
 
             else
@@ -78,26 +78,25 @@ modal model isOpen =
 
         backgroundOverlay : List Css.Style
         backgroundOverlay =
-            if isOpen then
-                [ Tw.transition_opacity, Tw.opacity_100, Tw.ease_in, Tw.duration_200 ]
+            if model.isOpen then
+                [ Tw.transition_opacity, Tw.ease_in, Tw.duration_200, Tw.opacity_100 ]
 
             else
-                [ Tw.transition_opacity, Tw.opacity_0, Tw.ease_out, Tw.duration_300 ]
+                [ Tw.transition_opacity, Tw.ease_out, Tw.duration_300, Tw.opacity_0 ]
 
         modalPanel : List Css.Style
         modalPanel =
-            if isOpen then
-                [ Tw.transition_all, Tw.opacity_100, Tw.translate_y_0, Bp.sm [ Tw.scale_100 ], Tw.ease_in, Tw.duration_200 ]
+            if model.isOpen then
+                [ Tw.transition_all, Tw.ease_in, Tw.duration_200, Tw.opacity_100, Tw.translate_y_0, Bp.sm [ Tw.scale_100 ] ]
 
             else
-                [ Tw.transition_all, Tw.opacity_0, Tw.translate_y_4, Bp.sm [ Tw.translate_y_0, Tw.scale_95 ], Tw.ease_out, Tw.duration_300 ]
+                [ Tw.transition_all, Tw.ease_out, Tw.duration_300, Tw.opacity_0, Tw.translate_y_4, Bp.sm [ Tw.translate_y_0, Tw.scale_95 ] ]
     in
     div [ ariaLabelledby model.id, role "dialog", ariaModal True, css ([ Tw.fixed, Tw.z_10, Tw.inset_0, Tw.overflow_y_auto ] ++ modalContainer) ]
         [ div [ css [ Tw.flex, Tw.items_end, Tw.justify_center, Tw.min_h_screen, Tw.pt_4, Tw.px_4, Tw.pb_20, Tw.text_center, Bp.sm [ Tw.block, Tw.p_0 ] ] ]
             [ div [ ariaHidden True, onClick model.onBackgroundClick, css ([ Tw.fixed, Tw.inset_0, Tw.bg_gray_500, Tw.bg_opacity_75 ] ++ backgroundOverlay) ] []
             , {- This element is to trick the browser into centering the modal contents. -} span [ css [ Tw.hidden, Bp.sm [ Tw.inline_block, Tw.align_middle, Tw.h_screen ] ], ariaHidden True ] [ text "\u{200B}" ]
-            , div [ css ([ Tw.inline_block, Tw.align_bottom, Tw.bg_white, Tw.rounded_lg, Tw.px_4, Tw.pt_5, Tw.pb_4, Tw.text_left, Tw.overflow_hidden, Tw.shadow_xl, Tw.transform, Bp.sm [ Tw.my_8, Tw.align_middle, Tw.max_w_lg, Tw.w_full, Tw.p_6 ] ] ++ modalPanel) ]
-                model.content
+            , div [ css ([ Tw.inline_block, Tw.align_bottom, Tw.bg_white, Tw.rounded_lg, Tw.px_4, Tw.pt_5, Tw.pb_4, Tw.text_left, Tw.overflow_hidden, Tw.shadow_xl, Tw.transform, Bp.sm [ Tw.my_8, Tw.align_middle, Tw.max_w_lg, Tw.w_full, Tw.p_6 ] ] ++ modalPanel) ] content
             ]
         ]
 
@@ -162,10 +161,10 @@ doc =
                         [ Button.primary3 Indigo [ onClick (setIsOpen True) ] [ text "Click me!" ]
                         , modal
                             { id = "modal-title"
+                            , isOpen = isOpen
                             , onBackgroundClick = setIsOpen False
-                            , content = [ text "Hello!" ]
                             }
-                            isOpen
+                            [ text "Hello!" ]
                         ]
                 )
             ]
