04bb1d1268a7e309a404e87c38af9eb234ab9540
Loïc Knuchel <loic.knuchel@doctolib.com>
2022-01-23 08:58:06 +0100

    handle layout messages

--- src/PagesComponents/Projects/Id_/Models/Erd.elm
+++ src/PagesComponents/Projects/Id_/Models/Erd.elm
@@ -1,8 +1,8 @@
-module PagesComponents.Projects.Id_.Models.Erd exposing (Erd, create, getColumn, getColumnProps, initTable, isShown, unpack)
+module PagesComponents.Projects.Id_.Models.Erd exposing (Erd, create, createLayout, getColumn, getColumnProps, initTable, isShown, unpack, unpackLayout)
 
 import Dict exposing (Dict)
-import Libs.Dict as D
-import Libs.Maybe as M
+import Libs.Dict as Dict
+import Libs.Maybe as Maybe
 import Libs.Ned as Ned
 import Models.Project exposing (Project)
 import Models.Project.CanvasProps exposing (CanvasProps)
@@ -28,6 +28,7 @@ type alias Erd =
     , canvas : CanvasProps
     , tables : Dict TableId ErdTable
     , relations : List ErdRelation
+    , relationsByTable : Dict TableId (List Relation)
     , tableProps : Dict TableId ErdTableProps
     , shownTables : List TableId
     , usedLayout : Maybe LayoutName
@@ -41,31 +42,31 @@ type alias Erd =
 create : List Project -> Project -> Erd
 create allProjects project =
     let
-        layoutProps : List TableProps
-        layoutProps =
-            project.layout.tables ++ project.layout.hiddenTables
-
         relationsByTable : Dict TableId (List Relation)
         relationsByTable =
             project.relations
                 |> List.foldr
                     (\rel dict ->
                         if rel.src.table == rel.ref.table then
-                            dict |> Dict.update rel.src.table (M.mapOrElse (\relations -> rel :: relations) [ rel ] >> Just)
+                            dict |> Dict.update rel.src.table (Maybe.mapOrElse (\relations -> rel :: relations) [ rel ] >> Just)
 
                         else
                             dict
-                                |> Dict.update rel.src.table (M.mapOrElse (\relations -> rel :: relations) [ rel ] >> Just)
-                                |> Dict.update rel.ref.table (M.mapOrElse (\relations -> rel :: relations) [ rel ] >> Just)
+                                |> Dict.update rel.src.table (Maybe.mapOrElse (\relations -> rel :: relations) [ rel ] >> Just)
+                                |> Dict.update rel.ref.table (Maybe.mapOrElse (\relations -> rel :: relations) [ rel ] >> Just)
                     )
                     Dict.empty
+
+        ( canvas, tableProps, shownTables ) =
+            createLayout relationsByTable project.layout
     in
     { project = ProjectInfo.create project
-    , canvas = project.layout.canvas
-    , tables = project.tables |> Dict.map (\id -> ErdTable.create project.tables (relationsByTable |> D.getOrElse id []))
+    , canvas = canvas
+    , tables = project.tables |> Dict.map (\id -> ErdTable.create project.tables (relationsByTable |> Dict.getOrElse id []))
     , relations = project.relations |> List.map (ErdRelation.create project.tables)
-    , tableProps = layoutProps |> List.map (\p -> ( p.id, ErdTableProps.create (relationsByTable |> D.getOrElse p.id []) (project.layout.tables |> List.map .id) p )) |> Dict.fromList
-    , shownTables = project.layout.tables |> List.map .id
+    , relationsByTable = relationsByTable
+    , tableProps = tableProps
+    , shownTables = shownTables
     , usedLayout = project.usedLayout
     , layouts = project.layouts
     , sources = project.sources
@@ -77,26 +78,47 @@ create allProjects project =
 unpack : Erd -> Project
 unpack erd =
     let
-        ( shownTables, hiddenTables ) =
-            erd.tableProps |> Dict.keys |> List.partition (\id -> erd.shownTables |> List.member id)
+        ( layoutCreatedAt, layoutUpdatedAt ) =
+            erd.usedLayout |> Maybe.andThen (\l -> erd.layouts |> Dict.get l) |> Maybe.mapOrElse (\l -> ( l.createdAt, l.updatedAt )) ( Time.millisToPosix 0, Time.millisToPosix 0 )
     in
     { id = erd.project.id
     , name = erd.project.name
     , sources = erd.sources
     , tables = erd.tables |> Dict.map (\_ -> ErdTable.unpack)
     , relations = erd.relations |> List.map ErdRelation.unpack
-    , layout =
-        { canvas = erd.canvas
-        , tables = shownTables |> List.filterMap (ErdTableProps.unpack erd.tableProps)
-        , hiddenTables = hiddenTables |> List.filterMap (ErdTableProps.unpack erd.tableProps)
-        , createdAt = Time.millisToPosix 0
-        , updatedAt = Time.millisToPosix 0
-        }
+    , layout = unpackLayout erd.canvas erd.tableProps erd.shownTables layoutCreatedAt layoutUpdatedAt
     , usedLayout = erd.usedLayout
     , layouts = erd.layouts
     , settings = erd.settings
     , createdAt = erd.project.createdAt
     , updatedAt = erd.project.updatedAt
+    }
+
+
+createLayout : Dict TableId (List Relation) -> Layout -> ( CanvasProps, Dict TableId ErdTableProps, List TableId )
+createLayout relationsByTable layout =
+    let
+        layoutProps : List TableProps
+        layoutProps =
+            layout.tables ++ layout.hiddenTables
+    in
+    ( layout.canvas
+    , layoutProps |> List.map (\p -> ( p.id, ErdTableProps.create (relationsByTable |> Dict.getOrElse p.id []) (layout.tables |> List.map .id) p )) |> Dict.fromList
+    , layout.tables |> List.map .id
+    )
+
+
+unpackLayout : CanvasProps -> Dict TableId ErdTableProps -> List TableId -> Time.Posix -> Time.Posix -> Layout
+unpackLayout canvas tableProps shownTables createdAt updatedAt =
+    let
+        ( tables, hiddenTables ) =
+            tableProps |> Dict.keys |> List.partition (\id -> shownTables |> List.member id)
+    in
+    { canvas = canvas
+    , tables = tables |> List.filterMap (ErdTableProps.unpack tableProps)
+    , hiddenTables = hiddenTables |> List.filterMap (ErdTableProps.unpack tableProps)
+    , createdAt = createdAt
+    , updatedAt = updatedAt
     }
 
 
--- src/PagesComponents/Projects/Id_/Updates/Layout.elm
+++ src/PagesComponents/Projects/Id_/Updates/Layout.elm
@@ -2,25 +2,27 @@ module PagesComponents.Projects.Id_.Updates.Layout exposing (Model, handleLayout
 
 import Conf
 import Dict
-import Libs.Maybe as M
+import Libs.Maybe as Maybe
 import Libs.Task as T
-import Models.Project exposing (Project)
+import Models.Project.Layout exposing (Layout)
 import Models.Project.LayoutName exposing (LayoutName)
-import PagesComponents.Projects.Id_.Models exposing (LayoutDialog, LayoutMsg(..), Msg(..), toastSuccess)
+import PagesComponents.Projects.Id_.Models exposing (LayoutDialog, LayoutMsg(..), Msg(..), toastError, toastSuccess)
+import PagesComponents.Projects.Id_.Models.Erd as Erd exposing (Erd)
 import Ports
-import Services.Lenses exposing (mapLayouts, mapNewLayoutM, mapProjectM, mapProjectMCmd, mapUsedLayout, setLayout, setName, setNewLayout, setUsedLayout)
+import Services.Lenses exposing (mapErdM, mapErdMCmd, mapLayouts, mapNewLayoutM, mapUsedLayout, setCanvas, setName, setNewLayout, setShownTables, setTableProps, setUsedLayout)
+import Time
 import Track
 
 
 type alias Model x =
     { x
         | newLayout : Maybe LayoutDialog
-        , project : Maybe Project
+        , erd : Maybe Erd
     }
 
 
-handleLayout : LayoutMsg -> Model x -> ( Model x, Cmd Msg )
-handleLayout msg model =
+handleLayout : Time.Posix -> LayoutMsg -> Model x -> ( Model x, Cmd Msg )
+handleLayout now msg model =
     case msg of
         LOpen ->
             ( model |> setNewLayout (Just { id = Conf.ids.newLayoutDialog, name = "" }), Cmd.batch [ T.sendAfter 1 (ModalOpen Conf.ids.newLayoutDialog), Ports.track Track.openSaveLayout ] )
@@ -29,63 +31,87 @@ handleLayout msg model =
             ( model |> mapNewLayoutM (setName name), Cmd.none )
 
         LCreate name ->
-            model |> setNewLayout Nothing |> mapProjectMCmd (createLayout name)
+            model |> setNewLayout Nothing |> mapErdMCmd (createLayout name now)
 
         LCancel ->
             ( model |> setNewLayout Nothing, Cmd.none )
 
         LLoad name ->
-            model |> mapProjectMCmd (loadLayout name)
+            model |> mapErdMCmd (loadLayout name)
 
         LUnload ->
-            ( model |> mapProjectM unloadLayout, Cmd.none )
+            ( model |> mapErdM unloadLayout, Cmd.none )
 
         LUpdate name ->
-            model |> mapProjectMCmd (updateLayout name)
+            model |> mapErdMCmd (updateLayout name now)
 
         LDelete name ->
-            model |> mapProjectMCmd (deleteLayout name)
+            model |> mapErdMCmd (deleteLayout name)
 
 
-createLayout : LayoutName -> Project -> ( Project, Cmd Msg )
-createLayout name project =
+createLayout : LayoutName -> Time.Posix -> Erd -> ( Erd, Cmd Msg )
+createLayout name now erd =
     -- TODO check that layout name does not already exist
-    project
-        |> setUsedLayout (Just name)
-        |> mapLayouts (Dict.insert name project.layout)
-        |> (\newSchema -> ( newSchema, Ports.track (Track.createLayout project.layout) ))
+    erd.usedLayout
+        |> Maybe.andThen (\l -> erd.layouts |> Dict.get l)
+        |> Maybe.mapOrElse
+            (\_ -> ( erd, Cmd.batch [ T.send (toastError ("Layout " ++ name ++ " already exists")) ] ))
+            (let
+                layout : Layout
+                layout =
+                    Erd.unpackLayout erd.canvas erd.tableProps erd.shownTables now now
+             in
+             erd
+                |> setUsedLayout (Just name)
+                |> mapLayouts (Dict.insert name layout)
+                |> (\newErd -> ( newErd, Ports.track (Track.createLayout layout) ))
+            )
 
 
-loadLayout : LayoutName -> Project -> ( Project, Cmd Msg )
-loadLayout name project =
-    project.layouts
+loadLayout : LayoutName -> Erd -> ( Erd, Cmd Msg )
+loadLayout name erd =
+    erd.layouts
         |> Dict.get name
-        |> M.mapOrElse
+        |> Maybe.mapOrElse
             (\layout ->
-                ( project |> setUsedLayout (Just name) |> setLayout layout
-                , Cmd.batch [ layout.tables |> List.map .id |> Ports.observeTablesSize, Ports.track (Track.loadLayout layout) ]
+                let
+                    ( canvas, tableProps, shownTables ) =
+                        Erd.createLayout erd.relationsByTable layout
+                in
+                ( erd |> setUsedLayout (Just name) |> setCanvas canvas |> setTableProps tableProps |> setShownTables shownTables
+                , Cmd.batch [ Ports.observeTablesSize shownTables, Ports.track (Track.loadLayout layout) ]
                 )
             )
-            ( project, Cmd.none )
+            ( erd, Cmd.none )
 
 
-unloadLayout : Project -> Project
-unloadLayout project =
-    project |> setUsedLayout Nothing
+unloadLayout : Erd -> Erd
+unloadLayout erd =
+    erd |> setUsedLayout Nothing
 
 
-updateLayout : LayoutName -> Project -> ( Project, Cmd Msg )
-updateLayout name project =
-    -- TODO check that layout name already exist
-    project
-        |> setUsedLayout (Just name)
-        |> mapLayouts (Dict.insert name project.layout)
-        |> (\newSchema -> ( newSchema, Cmd.batch [ T.send (toastSuccess ("Saved to layout " ++ name)), Ports.track (Track.updateLayout project.layout) ] ))
+updateLayout : LayoutName -> Time.Posix -> Erd -> ( Erd, Cmd Msg )
+updateLayout name now erd =
+    erd.usedLayout
+        |> Maybe.andThen (\l -> erd.layouts |> Dict.get l)
+        |> Maybe.mapOrElse
+            (\layout ->
+                let
+                    newLayout : Layout
+                    newLayout =
+                        Erd.unpackLayout erd.canvas erd.tableProps erd.shownTables layout.createdAt now
+                in
+                erd
+                    |> setUsedLayout (Just name)
+                    |> mapLayouts (Dict.insert name newLayout)
+                    |> (\newErd -> ( newErd, Cmd.batch [ T.send (toastSuccess ("Saved to layout " ++ name)), Ports.track (Track.updateLayout newLayout) ] ))
+            )
+            ( erd, Cmd.batch [ T.send (toastError ("Can't find layout " ++ name)), Ports.track (Track.notFoundLayout name) ] )
 
 
-deleteLayout : LayoutName -> Project -> ( Project, Cmd Msg )
-deleteLayout name project =
-    (project.layouts |> Dict.get name)
-        |> M.mapOrElse
-            (\l -> ( project |> mapUsedLayout (M.filter (\n -> n /= name)) |> mapLayouts (Dict.remove name), Ports.track (Track.deleteLayout l) ))
-            ( project, Cmd.none )
+deleteLayout : LayoutName -> Erd -> ( Erd, Cmd Msg )
+deleteLayout name erd =
+    (erd.layouts |> Dict.get name)
+        |> Maybe.mapOrElse
+            (\l -> ( erd |> mapUsedLayout (Maybe.filter (\n -> n /= name)) |> mapLayouts (Dict.remove name), Ports.track (Track.deleteLayout l) ))
+            ( erd, Cmd.none )
