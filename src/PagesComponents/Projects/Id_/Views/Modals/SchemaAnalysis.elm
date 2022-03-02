476c799d383ff765e2665e20c8bf079d0f98929f
Loïc Knuchel <loic.knuchel@doctolib.com>
2022-03-02 23:30:45 +0100

    improve missing relations in schema analysis

--- src/PagesComponents/Projects/Id_/Views/Modals/SchemaAnalysis.elm
+++ src/PagesComponents/Projects/Id_/Views/Modals/SchemaAnalysis.elm
@@ -4,18 +4,21 @@ import Components.Atoms.Button as Button
 import Components.Atoms.Icon as Icon exposing (Icon(..))
 import Components.Molecules.Modal as Modal
 import Dict exposing (Dict)
-import Html exposing (Html, div, h3, h4, p, span, text)
+import Html exposing (Html, div, h3, h4, h5, p, span, text)
 import Html.Attributes exposing (class, id)
 import Html.Events exposing (onClick)
-import Libs.Dict as Dict
+import Libs.Bool as B
 import Libs.Html.Attributes exposing (css)
+import Libs.List as List
 import Libs.Models.HtmlId exposing (HtmlId)
 import Libs.Ned as Ned
 import Libs.Nel as Nel
+import Libs.Regex as Regex
 import Libs.String as String
 import Libs.Tailwind as Tw exposing (sm)
-import Models.Project.ColumnName as ColumnName
+import Models.Project.ColumnName as ColumnName exposing (ColumnName)
 import Models.Project.ColumnRef as ColumnRef exposing (ColumnRef)
+import Models.Project.ColumnType exposing (ColumnType)
 import Models.Project.TableId as TableId exposing (TableId)
 import Models.Project.TableName exposing (TableName)
 import PagesComponents.Projects.Id_.Models exposing (Msg(..), SchemaAnalysisDialog, SchemaAnalysisMsg(..))
@@ -56,14 +59,102 @@ viewAnalysis tables =
         [ viewMissingRelations tables ]
 
 
+viewFooter : Html Msg
+viewFooter =
+    div [ class "px-6 py-3 mt-3 flex items-center justify-between flex-row-reverse bg-gray-50" ]
+        [ Button.primary3 Tw.primary [ onClick (ModalClose (SchemaAnalysisMsg SAClose)) ] [ text "Close" ] ]
+
+
+
+-- MISSING RELATIONS
+
+
+type alias ColumnInfo =
+    { table : TableId, column : ColumnName, kind : ColumnType }
+
+
+type alias MissingRelation =
+    { src : ColumnInfo
+    , ref : ColumnInfo
+    }
+
+
+type alias MissingRef =
+    { src : ColumnInfo
+    }
+
+
+infoToRef : ColumnInfo -> ColumnRef
+infoToRef info =
+    { table = info.table, column = info.column }
+
+
+computeMissingRelationColumns : Dict TableId ErdTable -> ( List MissingRelation, List MissingRef )
+computeMissingRelationColumns tables =
+    tables
+        |> Dict.values
+        |> List.concatMap
+            (\t ->
+                t.columns
+                    |> Ned.values
+                    |> Nel.toList
+                    |> List.filter (\c -> (c.name |> String.toLower |> Regex.match "_ids?$") && not c.isPrimaryKey && (c.inRelations |> List.isEmpty) && (c.outRelations |> List.isEmpty))
+                    |> List.map (\c -> { table = t.id, column = c.name, kind = c.kind })
+            )
+        |> List.map (\src -> ( src, tables |> getRef src ))
+        |> List.partition (\( _, maybeRef ) -> maybeRef /= Nothing)
+        |> Tuple.mapFirst (List.filterMap (\( src, maybeRef ) -> maybeRef |> Maybe.map (\ref -> { src = src, ref = ref })))
+        |> Tuple.mapSecond (List.map (\( src, _ ) -> { src = src }))
+
+
+getRef : ColumnInfo -> Dict TableId ErdTable -> Maybe ColumnInfo
+getRef src tables =
+    let
+        words : List String
+        words =
+            src.column |> String.toLower |> String.split "_" |> List.dropRight 1
+
+        prefix : TableName
+        prefix =
+            words |> String.join "_"
+
+        tableNames : List TableName
+        tableNames =
+            [ words |> String.join "_" |> String.plural, words |> List.drop 1 |> String.join "_" |> String.plural ]
+    in
+    tables
+        |> Dict.filter (\( schema, table ) _ -> (schema == Tuple.first src.table) && (table /= Tuple.second src.table) && (tableNames |> List.member (String.toLower table)))
+        |> Dict.values
+        |> List.sortBy (\t -> 0 - String.length t.name)
+        |> List.head
+        |> Maybe.andThen
+            (\t ->
+                t.columns
+                    |> Ned.find (\name _ -> String.toLower name == "id" || String.toLower name == (prefix ++ "_id"))
+                    |> Maybe.map (\( _, c ) -> { table = t.id, column = c.name, kind = c.kind })
+            )
+
+
+kindMatch : MissingRelation -> Bool
+kindMatch rel =
+    if (rel.src.column |> String.toLower |> String.endsWith "_ids") && (rel.src.kind |> String.endsWith "[]") then
+        (rel.src.kind |> String.dropRight 2) == rel.ref.kind
+
+    else
+        rel.src.kind == rel.ref.kind
+
+
 viewMissingRelations : Dict TableId ErdTable -> Html Msg
 viewMissingRelations tables =
     let
-        missingRels : List MissingRelation
-        missingRels =
+        ( missingRels, missingRefs ) =
             tables |> computeMissingRelationColumns
+
+        count : Int
+        count =
+            (missingRels |> List.length) + (missingRefs |> List.length)
     in
-    if missingRels |> List.isEmpty then
+    if count == 0 then
         div [ class "mt-3" ]
             [ h4 [ class "leading-5 font-medium" ]
                 [ Icon.solid Check "inline mr-3 text-green-500"
@@ -75,7 +166,7 @@ viewMissingRelations tables =
         div [ class "mt-3" ]
             [ h4 [ class "leading-5 font-medium" ]
                 [ Icon.solid LightBulb "inline mr-3 text-yellow-500"
-                , text ("Found " ++ (missingRels |> String.pluralizeL "potentially missing relation"))
+                , text ("Found " ++ (count |> String.pluralize "potentially missing relation"))
                 ]
             , div []
                 (missingRels
@@ -90,60 +181,35 @@ viewMissingRelations tables =
                                     , text (TableId.show rel.src.table)
                                     , span [ class "text-gray-500" ] [ text (ColumnName.withName rel.src.column "") ]
                                     ]
-                                , Button.primary3 Tw.primary [ class "ml-3", onClick (CreateRelation rel.src rel.ref) ] [ text "Add relation" ]
+                                , div [ class "ml-3" ]
+                                    [ B.cond (kindMatch rel) (span [] []) (span [ class "text-gray-400 mr-3" ] [ Icon.solid Exclamation "inline", text (" " ++ rel.ref.kind ++ " vs " ++ rel.src.kind) ])
+                                    , Button.primary3 Tw.primary [ onClick (CreateRelation (infoToRef rel.src) (infoToRef rel.ref)) ] [ text "Add relation" ]
+                                    ]
                                 ]
                         )
                 )
+            , if missingRefs |> List.isEmpty then
+                div [] []
+
+              else
+                div []
+                    [ h5 [ class "mt-1 font-medium" ] [ text "Some columns may need a relation, but can't find a related table:" ]
+                    , div []
+                        (missingRefs
+                            |> List.map
+                                (\rel ->
+                                    div [ class "ml-3" ]
+                                        [ text (TableId.show rel.src.table)
+                                        , span [ class "text-gray-500" ] [ text (ColumnName.withName rel.src.column "") ]
+                                        , span [ class "text-gray-400" ] [ text (" (" ++ rel.src.kind ++ ")") ]
+                                        ]
+                                )
+                        )
+                    ]
             ]
 
 
-viewFooter : Html Msg
-viewFooter =
-    div [ class "px-6 py-3 mt-3 flex items-center justify-between flex-row-reverse bg-gray-50" ]
-        [ Button.primary3 Tw.primary [ onClick (ModalClose (SchemaAnalysisMsg SAClose)) ] [ text "Close" ] ]
 
-
-
--- ANALYSIS
-
-
-type alias MissingRelation =
-    { src : ColumnRef
-    , ref : ColumnRef
-    }
-
-
-computeMissingRelationColumns : Dict TableId ErdTable -> List MissingRelation
-computeMissingRelationColumns tables =
-    tables
-        |> Dict.values
-        |> List.concatMap
-            (\t ->
-                t.columns
-                    |> Ned.values
-                    |> Nel.toList
-                    |> List.filter (\c -> (c.name |> String.toLower |> String.endsWith "_id") && (c.inRelations |> List.isEmpty) && (c.outRelations |> List.isEmpty))
-                    |> List.map (\c -> { table = t.id, column = c.name })
-            )
-        |> List.filterMap (\src -> tables |> getRef src |> Maybe.map (\ref -> { src = src, ref = ref }))
-
-
-getRef : ColumnRef -> Dict TableId ErdTable -> Maybe ColumnRef
-getRef col tables =
-    let
-        prefix : TableName
-        prefix =
-            col.column |> String.toLower |> String.dropRight 3
-
-        tableName : TableName
-        tableName =
-            prefix |> String.plural
-    in
-    tables
-        |> Dict.find (\( schema, table ) _ -> (schema == Tuple.first col.table) && (String.toLower table == tableName))
-        |> Maybe.andThen
-            (\( _, t ) ->
-                t.columns
-                    |> Ned.find (\name _ -> String.toLower name == "id" || String.toLower name == (prefix ++ "_id"))
-                    |> Maybe.map (\( _, c ) -> { table = t.id, column = c.name })
-            )
+-- suggest to split tables with more than 50 columns
+-- identify columns with same names but different types (bad homogeneity)
+-- '_at' columns not of date type
