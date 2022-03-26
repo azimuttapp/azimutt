1d94a1c8ebdf87a0a4e89096d52ca07910d04493
Samir Bouaked <sbouaked@pm.me>
2022-03-26 18:17:02 +0100

    fix(vite): setup double endpoint

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,14 @@
-import { defineConfig } from "vite"
 import elmPlugin from 'vite-plugin-elm'
+const { resolve } = require('path')
 
-export default defineConfig({
-    plugins: [elmPlugin()]
-})
+export default {
+    plugins: [elmPlugin()],
+    build: {
+        rollupOptions: {
+          input: {
+            main: resolve(__dirname, 'index.html'),
+            nested: resolve(__dirname, 'book.html')
+          }
+        }
+      }
+}
