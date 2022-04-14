b2bf318519c01837525ec5a991df46cd5cc4c56d
Loïc Knuchel <loic.knuchel@doctolib.com>
2022-04-14 13:28:04 +0200

    fix tailwind config replaceAll

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ const colors = require('tailwindcss/colors')
 const usedColors = ['primary', 'default', 'slate', 'gray', 'red', 'orange', 'amber', 'yellow', 'lime', 'green', 'emerald', 'teal', 'cyan', 'sky', 'blue', 'indigo', 'violet', 'purple', 'fuchsia', 'pink', 'rose']
 function expandDynamicColors(fileContent) {
     // see "DYNAMIC COLOR CLASSES" in src/Libs/Tailwind.elm
-    return fileContent.replaceAll(/([^_\n])(bg|border|fill|ring|ring_offset|stroke|text)_([0-9]{1,2}0) [^ ,):\n]+/g, (match, start, attr, level) => {
+    return fileContent.replace(/([^_\n])(bg|border|fill|ring|ring_offset|stroke|text)_([0-9]{1,2}0) [^ ,):\n]+/g, (match, start, attr, level) => {
         return `${start}"${usedColors.map(clazz => `${attr}-${clazz}-${level}`).join(' ')}"`
     })
 }
@@ -11,7 +11,7 @@ function expandDynamicColors(fileContent) {
 const stateMappings = {xxl: '2xl', focusWithin: 'focus-within'}
 function expandDynamicStates(fileContent) {
     // see "DYNAMIC STATE CLASSES" in src/Libs/Tailwind.elm
-    return fileContent.replaceAll(/(?:Tw\.)?(sm|md|lg|xl|xxl|hover|focus|active|disabled|focusWithin) \[ ([^\]]+) ]/g, (match, state, content) => {
+    return fileContent.replace(/(?:Tw\.)?(sm|md|lg|xl|xxl|hover|focus|active|disabled|focusWithin) \[ ([^\]]+) ]/g, (match, state, content) => {
         const stateClass = stateMappings[state] || state
         const classes = [...content.matchAll(/"([^"]+)"/g)].map(match => match[1]) // collect string and concat them
         return `"${classes.join(' ').split(' ').map(c => c.trim()).filter(c => c !== '').map(clazz => stateClass + ':' + clazz).join(' ')}"`
