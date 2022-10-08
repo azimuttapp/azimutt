721e1523578d1ba36ec27ee2f82e650e6c9126f6
Loïc Knuchel <loic.knuchel@doctolib.com>
2022-10-08 15:49:33 +0200

    logs for blog

--- backend/lib/azimutt/blog.ex
+++ backend/lib/azimutt/blog.ex
@@ -9,11 +9,18 @@ defmodule Azimutt.Blog do
 
   def get_articles do
     # FIXME: add caching for articles
-    Path.wildcard("#{article_path()}/????-??-??-*/*.md")
-    |> Enum.map(&Article.path_to_id/1)
-    |> Enum.map(&get_article/1)
-    |> Result.sequence()
-    |> Result.map(fn articles -> articles |> Enum.sort_by(& &1.published, {:desc, Date}) end)
+    IO.inspect("get_articles")
+    paths = Path.wildcard("#{article_path()}/????-??-??-*/*.md")
+    IO.inspect(paths, label: "paths")
+    ids = paths |> Enum.map(&Article.path_to_id/1)
+    IO.inspect(ids, label: "ids")
+    articles = ids |> Enum.map(&get_article/1)
+    IO.inspect(articles |> Enum.map(fn r -> r |> Result.map(fn a -> a.id end) end), label: "articles")
+    result = articles |> Result.sequence()
+    IO.inspect(result |> Result.map(fn r -> r |> Enum.map(fn a -> a.id end) end), label: "result")
+    sorted = result |> Result.map(fn articles -> articles |> Enum.sort_by(& &1.published, {:desc, Date}) end)
+    IO.inspect(sorted |> Result.map(fn r -> r |> Enum.map(fn a -> a.id end) end), label: "sorted")
+    sorted
   end
 
   def get_article(id) do
