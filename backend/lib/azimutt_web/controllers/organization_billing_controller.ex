26d1a65efa6d5f26ee071116342ceba9c9b43734
Samir Bouaked <sbouaked@pm.me>
2022-10-09 15:40:48 +0200

    fix merge

--- backend/lib/azimutt_web/controllers/organization_billing_controller.ex
+++ backend/lib/azimutt_web/controllers/organization_billing_controller.ex
@@ -6,16 +6,12 @@ defmodule AzimuttWeb.OrganizationBillingController do
   def index(conn, %{"organization_id" => id}) do
     current_user = conn.assigns.current_user
 
-<<<<<<< HEAD
-    with {:ok, %Organization{} = organization} <- Organizations.get_organization(id, current_user),
-         do: conn |> put_view(AzimuttWeb.OrganizationView) |> render("billing.html", organization: organization, active_plan: :free)
-=======
+
     with {:ok, %Organization{} = organization} <- Organizations.get_organization(id, current_user) do
       with {:ok, current_subscription} <- Stripe.Subscription.retrieve("sub_1LqfILCaPXsf4vehL174ZNlf") do
         conn |> put_view(AzimuttWeb.OrganizationView) |> render("billing.html", organization: organization, active_plan: current_subscription.plan.active)
       end
     end
 
->>>>>>> 38542dd (update billing controller)
   end
 end
