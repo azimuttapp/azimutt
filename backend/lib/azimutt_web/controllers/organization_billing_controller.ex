72254f34cf7afb867710374f4ee881b784557913
Samir Bouaked <sbouaked@pm.me>
2022-10-09 16:51:45 +0200

    push avant loic

--- backend/lib/azimutt_web/controllers/organization_billing_controller.ex
+++ backend/lib/azimutt_web/controllers/organization_billing_controller.ex
@@ -7,11 +7,27 @@ defmodule AzimuttWeb.OrganizationBillingController do
     current_user = conn.assigns.current_user
 
     with {:ok, %Organization{} = organization} <- Organizations.get_organization(id, current_user) do
-      if
+      if organization.stripe_subscription_id do
+        status = Organizations.get_subscription_status(organization.stripe_subscription_id)
 
-      conn
-      |> put_view(AzimuttWeb.OrganizationView)
-      |> render("billing.html", organization: organization, active_plan: current_subscription.plan.active)
+        case status do
+          :active -> generate_billing_view(conn, "billing.html", organization, "Your subscription is active !")
+          :past_due -> generate_billing_view(conn, "billing.html", organization, "We have an issue with your subscription")
+          :unpaid -> generate_billing_view(conn, "billing.html", organization, "We have an issue with your subscription")
+          :canceled -> generate_billing_view(conn, "billing.html", organization, "Your subscription is canceled")
+          :incomplete -> generate_billing_view(conn, "billing.html", organization, "We have an issue with your subscription")
+          :incomplete_expired -> generate_billing_view(conn, "billing.html", organization, "We have an issue with your subscription")
+          :trialing -> generate_billing_view(conn, "billing.html", organization, "You are in free trial")
+        end
+      else
+        generate_billing_view(conn, "billing.html", organization, "You haven't got subscribe yet !")
+      end
     end
+  end
+
+  defp generate_billing_view(conn, file, organization, message) do
+    conn
+    |> put_view(AzimuttWeb.OrganizationView)
+    |> render(file, organization: organization, active_plan: :free, message: message)
   end
 end
