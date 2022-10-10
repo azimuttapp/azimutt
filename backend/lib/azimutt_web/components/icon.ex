f49db25050b35b1608a92c441ccbfb800964de2d
Loïc Knuchel <loic.knuchel@doctolib.com>
2022-10-10 19:45:39 +0200

    fix pricing

--- backend/lib/azimutt_web/components/icon.ex
+++ backend/lib/azimutt_web/components/icon.ex
@@ -5,6 +5,27 @@ defmodule AzimuttWeb.Components.Icon do
   # https://heroicons.com
   # https://hexdocs.pm/phoenix_live_view/Phoenix.Component.html
 
+  def arrow_left(assigns) do
+    classes = if assigns[:class], do: " #{assigns[:class]}", else: ""
+
+    case assigns[:kind] do
+      "outline" ->
+        ~H"""
+        <.outline classes={classes}><path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5L3 12m0 0l7.5-7.5M3 12h18" /></.outline>
+        """
+
+      "solid" ->
+        ~H"""
+        <.solid classes={classes}><path fill-rule="evenodd" d="M11.03 3.97a.75.75 0 010 1.06l-6.22 6.22H21a.75.75 0 010 1.5H4.81l6.22 6.22a.75.75 0 11-1.06 1.06l-7.5-7.5a.75.75 0 010-1.06l7.5-7.5a.75.75 0 011.06 0z" clip-rule="evenodd" /></.solid>
+        """
+
+      _ ->
+        ~H"""
+        <.mini classes={classes}><path fill-rule="evenodd" d="M17 10a.75.75 0 01-.75.75H5.612l4.158 3.96a.75.75 0 11-1.04 1.08l-5.5-5.25a.75.75 0 010-1.08l5.5-5.25a.75.75 0 111.04 1.08L5.612 9.25H16.25A.75.75 0 0117 10z" clip-rule="evenodd" /></.mini>
+        """
+    end
+  end
+
   def arrow_left_on_rectangle(assigns) do
     classes = if assigns[:class], do: " #{assigns[:class]}", else: ""
 
