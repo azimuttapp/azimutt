50b358fa5d36e0115716cd220d2bf42e64c7ff97
Samir Bouaked <sbouaked@pm.me>
2022-12-07 18:33:57 +0100

    make docker compose use dev env

--- backend/config/dev.exs
+++ backend/config/dev.exs
@@ -12,16 +12,6 @@ config :cors_plug,
   origin: ["http://localhost:4001"],
   max_age: 86400
 
-# Configure your database
-config :azimutt, Azimutt.Repo,
-  username: "postgres",
-  password: "postgres",
-  hostname: "localhost",
-  database: "azimutt_dev",
-  stacktrace: true,
-  show_sensitive_data_on_connection_error: true,
-  pool_size: 10
-
 # For development, we disable any cache and enable
 # debugging and code reloading.
 #
--- backend/config/runtime.exs
+++ backend/config/runtime.exs
@@ -49,49 +49,49 @@ else
   IO.puts("Stripe not setup (STRIPE_API_KEY and STRIPE_WEBHOOK_SIGNING_SECRET env variables not found)")
 end
 
+database_url =
+  System.get_env("POSTGRESQL_ADDON_URI") ||
+    raise """
+    environment variable POSTGRESQL_ADDON_URI is missing.
+    For example: ecto://USER:PASS@HOST/DATABASE
+    """
+
+maybe_ipv6 = if System.get_env("ECTO_IPV6"), do: [:inet6], else: []
+
+config :azimutt, Azimutt.Repo,
+  # ssl: true,
+  url: database_url,
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  socket_options: maybe_ipv6
+
+# The secret key base is used to sign/encrypt cookies and other secrets.
+# A default value is used in config/dev.exs and config/test.exs but you
+# want to use a different value for prod and you most likely don't want
+# to check this value into version control, so we use an environment
+# variable instead.
+secret_key_base =
+  System.get_env("SECRET_KEY_BASE") ||
+    raise """
+    environment variable SECRET_KEY_BASE is missing.
+    You can generate one by calling: mix phx.gen.secret
+    """
+
+host = System.get_env("PHX_HOST") || "azimutt.app"
+port = String.to_integer(System.get_env("PORT") || "8080")
+
+config :azimutt, AzimuttWeb.Endpoint,
+  url: [host: host, port: 443, scheme: "https"],
+  http: [
+    # Enable IPv6 and bind on all interfaces.
+    # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
+    # See the documentation on https://hexdocs.pm/plug_cowboy/Plug.Cowboy.html
+    # for details about using IPv6 vs IPv4 and loopback vs public addresses.
+    ip: {0, 0, 0, 0, 0, 0, 0, 0},
+    port: port
+  ],
+  secret_key_base: secret_key_base
+
 if config_env() == :prod || config_env() == :staging do
-  database_url =
-    System.get_env("POSTGRESQL_ADDON_URI") ||
-      raise """
-      environment variable POSTGRESQL_ADDON_URI is missing.
-      For example: ecto://USER:PASS@HOST/DATABASE
-      """
-
-  maybe_ipv6 = if System.get_env("ECTO_IPV6"), do: [:inet6], else: []
-
-  config :azimutt, Azimutt.Repo,
-    # ssl: true,
-    url: database_url,
-    pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
-    socket_options: maybe_ipv6
-
-  # The secret key base is used to sign/encrypt cookies and other secrets.
-  # A default value is used in config/dev.exs and config/test.exs but you
-  # want to use a different value for prod and you most likely don't want
-  # to check this value into version control, so we use an environment
-  # variable instead.
-  secret_key_base =
-    System.get_env("SECRET_KEY_BASE") ||
-      raise """
-      environment variable SECRET_KEY_BASE is missing.
-      You can generate one by calling: mix phx.gen.secret
-      """
-
-  host = System.get_env("PHX_HOST") || "azimutt.app"
-  port = String.to_integer(System.get_env("PORT") || "8080")
-
-  config :azimutt, AzimuttWeb.Endpoint,
-    url: [host: host, port: 443, scheme: "https"],
-    http: [
-      # Enable IPv6 and bind on all interfaces.
-      # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
-      # See the documentation on https://hexdocs.pm/plug_cowboy/Plug.Cowboy.html
-      # for details about using IPv6 vs IPv4 and loopback vs public addresses.
-      ip: {0, 0, 0, 0, 0, 0, 0, 0},
-      port: port
-    ],
-    secret_key_base: secret_key_base
-
   mailgun_api_key = System.get_env("MAILGUN_API_KEY")
   mailgun_domain = System.get_env("MAILGUN_DOMAIN")
   mailgun_base_url = System.get_env("MAILGUN_BASE_URL")
