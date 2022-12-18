4514d624047edfbed43f68de97e85ba4200739fd
Loïc Knuchel <loic.knuchel@doctolib.com>
2022-12-18 22:25:31 +0100

    improve common values

--- backend/lib/azimutt/analyzer/column_stats.ex
+++ backend/lib/azimutt/analyzer/column_stats.ex
@@ -8,12 +8,19 @@ defmodule Azimutt.Analyzer.ColumnStats do
     field :table, String.t()
     field :column, String.t()
     field :type, String.t()
-    field :rows, integer()
-    field :nulls, integer()
-    field :cardinality, integer()
+    field :rows, pos_integer()
+    field :nulls, pos_integer()
+    field :cardinality, pos_integer()
     # field :min, any()
     # field :max, any()
-    field :common_values, map()
+    field :common_values, list(ValueCount.t())
     # field :random_values, list(any())
+  end
+
+  typedstruct module: ValueCount, enforce: true do
+    @moduledoc false
+    @derive Jason.Encoder
+    field :value, any()
+    field :count, pos_integer()
   end
 end
