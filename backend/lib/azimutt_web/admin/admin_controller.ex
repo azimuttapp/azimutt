fa0dec776c18241ddcc81d28a214f4e3f493f8d9
Loïc Knuchel <loic.knuchel@doctolib.com>
2022-12-19 20:27:14 +0100

    make it work

--- backend/lib/azimutt_web/admin/admin_controller.ex
+++ backend/lib/azimutt_web/admin/admin_controller.ex
@@ -1,8 +1,8 @@
-defmodule AzimuttWeb.Admin.DashboardController do
+defmodule AzimuttWeb.Admin.AdminController do
   use AzimuttWeb, :controller
   action_fallback AzimuttWeb.FallbackController
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    conn |> render("index.html")
   end
 end
--- backend/lib/azimutt_web/admin/admin_view.ex
+++ backend/lib/azimutt_web/admin/admin_view.ex
@@ -1,4 +1,5 @@
-defmodule AzimuttWeb.Admin.DashboardView do
-  use AzimuttWeb, :view
+defmodule AzimuttWeb.Admin.AdminView do
+  # use AzimuttWeb, :view
   # use Phoenix.View, root: "lib/azimutt_web/admin/templates", path: "*"
+  use Phoenix.View, root: "lib/azimutt_web", namespace: AzimuttWeb
 end
