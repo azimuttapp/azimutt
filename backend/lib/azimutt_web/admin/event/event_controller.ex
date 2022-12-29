5448a9ce745ee957a5a7885d87fbb2d87b438386
Samir Bouaked <sbouaked@pm.me>
2022-12-29 18:34:28 +0100

    refacto

--- backend/lib/azimutt_web/admin/event/event_controller.ex
+++ backend/lib/azimutt_web/admin/event/event_controller.ex
@@ -2,6 +2,12 @@ defmodule AzimuttWeb.Admin.EventController do
   use AzimuttWeb, :controller
   action_fallback AzimuttWeb.FallbackController
 
+  def index(conn, _params) do
+    events = Azimutt.Admin.list_last_events()
+
+    render(conn, "index.html", events: events)
+  end
+
   def show(conn, %{"id" => event_id}) do
     with {:ok, event} <- Azimutt.Admin.get_event(event_id) do
       render(conn, "show.html", event: event)
--- backend/lib/azimutt_web/admin/event/event_view.ex
+++ backend/lib/azimutt_web/admin/event/event_view.ex
@@ -2,4 +2,63 @@ defmodule AzimuttWeb.Admin.EventView do
   # use AzimuttWeb, :view
   # use Phoenix.View, root: "lib/azimutt_web/admin/templates", path: "*"
   use Phoenix.View, root: "lib/azimutt_web", namespace: AzimuttWeb
+
+  import Phoenix.HTML.Tag
+  import Phoenix.HTML.Link
+  alias AzimuttWeb.Router.Helpers, as: Routes
+
+  def format_date(date) do
+    {:ok, date_parsed} = Timex.format(date, "{D}/{M}/{YY} à {h24}h{m}:{s}")
+    date_parsed
+  end
+
+  def display_project_name(event) do
+    if event.project !== nil do
+      content_tag(:div, event.project.name, class: "font-medium")
+    else
+      " -"
+    end
+  end
+
+  def display_organization_name(event) do
+    if event.organization !== nil do
+      event.organization.name
+    else
+      " - "
+    end
+  end
+
+  def badge(event_name) do
+    default_class = "px-1"
+
+    case event_name do
+      event_name when event_name in [:login] ->
+        content_tag(:div, event_name, class: default_class <> "text-gray-100")
+
+      event_name when event_name in [:project_updated] ->
+        content_tag(:div, event_name, class: default_class <> " text-scheme-blue")
+
+      event_name when event_name in [:project_loaded, :subscribe_init, :subscribe_start, :stripe_subscription_created, :billing_loaded] ->
+        content_tag(:div, event_name, class: default_class <> " text-scheme-yellow")
+
+      event_name when event_name in [:project_created, :subscribe_success, :stripe_invoice_paid, :stripe_subscription_renewed] ->
+        content_tag(:div, event_name, class: default_class <> " text-scheme-green")
+
+      event_name
+      when event_name in [
+             :project_deleted,
+             :subscribe_error,
+             :subscribe_abort,
+             :stripe_invoice_payment_failed,
+             :stripe_subscription_canceled
+           ] ->
+        content_tag(:div, event_name, class: default_class <> " text-scheme-red")
+
+      event_name when event_name in [:stripe_subscription_quantity_updated, :stripe_subscription_updated] ->
+        content_tag(:div, event_name, class: default_class <> " text-scheme-darkblue")
+
+      event_name ->
+        content_tag(:div, event_name, class: default_class <> " text-gray-200")
+    end
+  end
 end
