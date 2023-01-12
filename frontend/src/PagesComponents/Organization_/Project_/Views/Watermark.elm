a3ad117dc83efdfb91c921fe4424588c0199d468
Loïc Knuchel <loic.knuchel@doctolib.com>
2023-01-12 21:15:32 +0100

    blog: memo feature

--- frontend/src/PagesComponents/Organization_/Project_/Views/Watermark.elm
+++ frontend/src/PagesComponents/Organization_/Project_/Views/Watermark.elm
@@ -9,7 +9,7 @@ import Services.Backend as Backend
 
 viewWatermark : Html msg
 viewWatermark =
-    div [ class "az-watermark absolute bottom-0 left-0 m-6" ]
+    div [ class "az-watermark absolute bottom-0 left-0 m-3" ]
         [ extLink Conf.constants.azimuttWebsite
             [ class "flex justify-start items-center flex-shrink-0 grayscale opacity-50 hover:opacity-100" ]
             [ img [ class "block h-12 w-auto", src (Backend.resourceUrl "/logo_dark.svg"), alt "Azimutt" ] []
