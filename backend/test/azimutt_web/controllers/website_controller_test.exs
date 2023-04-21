32dad06048c4347025026e79a0f6b216ac6c60ea
Loïc Knuchel <loic.knuchel@doctolib.com>
2023-04-21 17:31:12 +0200

    skip site by default

--- backend/test/azimutt_web/controllers/website_controller_test.exs
+++ backend/test/azimutt_web/controllers/website_controller_test.exs
@@ -1,6 +1,14 @@
 defmodule AzimuttWeb.WebsiteControllerTest do
   use AzimuttWeb.ConnCase
 
+  setup do
+    Application.put_env(:azimutt, :skip_public_site, false)
+
+    on_exit(fn ->
+      Application.delete_env(:azimutt, :skip_public_site)
+    end)
+  end
+
   test "GET /", %{conn: conn} do
     conn = get(conn, Routes.website_path(conn, :index))
     assert html_response(conn, 200) =~ "Search on tables, columns, relations and comments."
