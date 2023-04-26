b0f23477eaa01cf4d08171773516f04cb01a5828
Loïc Knuchel <loic.knuchel@doctolib.com>
2023-04-26 22:18:24 +0200

    publish desktop app

---
+++ desktop/README.md
@@ -0,0 +1,17 @@
+# Azimutt Desktop
+
+## Development
+
+- run `npm start`
+
+## Packaging
+
+Setup:
+- install `rpmbuild`
+
+Build:
+- run `npm run make`
+
+## Publish
+
+- run `npm run publish`
--- desktop/forge.config.ts
+++ desktop/forge.config.ts
@@ -1,35 +1,60 @@
-import type { ForgeConfig } from '@electron-forge/shared-types';
-import { MakerSquirrel } from '@electron-forge/maker-squirrel';
-import { MakerZIP } from '@electron-forge/maker-zip';
-import { MakerDeb } from '@electron-forge/maker-deb';
-import { MakerRpm } from '@electron-forge/maker-rpm';
-import { WebpackPlugin } from '@electron-forge/plugin-webpack';
+import {MakerDeb} from '@electron-forge/maker-deb';
+import {MakerRpm} from '@electron-forge/maker-rpm';
+import {MakerSquirrel} from '@electron-forge/maker-squirrel';
+import {MakerZIP} from '@electron-forge/maker-zip';
+import {WebpackPlugin} from '@electron-forge/plugin-webpack';
+import PublisherGithub from "@electron-forge/publisher-github";
+import type {ForgeConfig} from '@electron-forge/shared-types';
+import {mainConfig} from './webpack.main.config';
+import {rendererConfig} from './webpack.renderer.config';
 
-import { mainConfig } from './webpack.main.config';
-import { rendererConfig } from './webpack.renderer.config';
-
+// TODO: https://www.electronjs.org/docs/latest/tutorial/tutorial-packaging#important-signing-your-code
+// TODO: see https://github.com/electron/fiddle/tree/fc906d7049f1e173c488fa53972d6db868132e84/.github/workflows for github workflow publish
 const config: ForgeConfig = {
-  packagerConfig: {},
-  rebuildConfig: {},
-  makers: [new MakerSquirrel({}), new MakerZIP({}, ['darwin']), new MakerRpm({}), new MakerDeb({})],
-  plugins: [
-    new WebpackPlugin({
-      mainConfig,
-      renderer: {
-        config: rendererConfig,
-        entryPoints: [
-          {
-            html: './src/renderer/index.html',
-            js: './src/renderer/index.ts',
-            name: 'main_window',
-            preload: {
-              js: './src/preload.ts',
+    rebuildConfig: {},
+    plugins: [
+        new WebpackPlugin({
+            mainConfig,
+            renderer: {
+                config: rendererConfig,
+                entryPoints: [
+                    {
+                        html: './src/renderer/index.html',
+                        js: './src/renderer/index.ts',
+                        name: 'main_window',
+                        preload: {
+                            js: './src/preload.ts',
+                        },
+                    },
+                ],
             },
-          },
-        ],
-      },
-    }),
-  ],
+        }),
+    ],
+    packagerConfig: {
+        /* osxSign: {},
+        osxNotarize: {
+            tool: 'notarytool',
+            appleId: process.env.APPLE_ID,
+            appleIdPassword: process.env.APPLE_PASSWORD,
+            teamId: process.env.APPLE_TEAM_ID,
+        } */
+    },
+    makers: [
+        new MakerSquirrel({
+            /* certificateFile: './cert.pfx',
+            certificatePassword: process.env.CERTIFICATE_PASSWORD */
+        }),
+        new MakerZIP({}, ['darwin']),
+        new MakerRpm({}),
+        new MakerDeb({})
+    ],
+    publishers: [
+        new PublisherGithub({
+            repository: {owner: 'azimuttapp', name: 'azimutt'},
+            prerelease: true,
+            draft: true,
+        })
+    ]
 };
 
 export default config;
