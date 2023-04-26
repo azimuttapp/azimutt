e0dd72b0b742314dc97d97869e5b53afc4ee2881
Loïc Knuchel <loic.knuchel@doctolib.com>
2023-04-26 09:38:35 +0200

    Setup TS

--- desktop/src/preload.ts
+++ desktop/src/preload.ts
@@ -1,7 +1,7 @@
 // See the Electron documentation for details on how to use preload scripts:
 // https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
 window.addEventListener('DOMContentLoaded', () => {
-  const replaceText = (selector, text) => {
+  const replaceText = (selector: string, text: string) => {
     const element = document.getElementById(selector)
     if (element) element.innerText = text
   }
