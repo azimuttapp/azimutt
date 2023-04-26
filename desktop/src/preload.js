72ae0d838efd165d7ba2b938492e94ab7e584b8e
Loïc Knuchel <loic.knuchel@doctolib.com>
2023-04-26 09:13:32 +0200

    add versions

--- desktop/src/preload.js
+++ desktop/src/preload.js
@@ -1,2 +1,12 @@
 // See the Electron documentation for details on how to use preload scripts:
 // https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
+window.addEventListener('DOMContentLoaded', () => {
+  const replaceText = (selector, text) => {
+    const element = document.getElementById(selector)
+    if (element) element.innerText = text
+  }
+
+  for (const dependency of ['chrome', 'node', 'electron']) {
+    replaceText(`${dependency}-version`, process.versions[dependency])
+  }
+})
