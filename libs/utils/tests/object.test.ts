3a1f5f7f5b675633994736b76e84063c2f498b4b
Loïc Knuchel <loic.knuchel@doctolib.com>
2023-04-29 15:33:09 +0200

    Adjust pre-commit

--- libs/utils/tests/object.test.ts
+++ libs/utils/tests/object.test.ts
@@ -5,6 +5,7 @@ describe('object', () => {
     test('mapValues', () => {
         expect(mapValues({a: 'luc', b: 'jean'}, v => v.length)).toEqual({a: 3, b: 4})
     })
+
     test('filterValues', () => {
         expect(filterValues({a: 1, b: 2, c: 3, d: 4}, v => v % 2 === 0)).toEqual({b: 2, d: 4})
     })
