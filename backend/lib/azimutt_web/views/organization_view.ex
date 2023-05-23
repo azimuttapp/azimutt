d5b5d9aa9b5694f9479c3b0fe0fe5592c9346a97
Samir Bouaked <sbouaked@pm.me>
2023-05-23 16:45:11 +0200

    use content tag to avoid unsafe code

--- backend/lib/azimutt_web/views/organization_view.ex
+++ backend/lib/azimutt_web/views/organization_view.ex
@@ -9,12 +9,12 @@ defmodule AzimuttWeb.OrganizationView do
   def generate_html_event_description(event) do
     %{text: text, author: author, destination: destination} = Azimutt.Tracking.event_to_action(event)
 
-    raw("""
-    <div class="text-xs">
-      <span class="font-semibold text-gray-900 "> #{author}</span>
-      <span class="text-gray-800"> #{text}</span>
-      <span class="font-semibold text-blue-500"> #{destination}</span>
-    </div>
-    """)
+    content_tag :div, class: "text-xs" do
+      [
+        content_tag(:span, " #{author}", class: "font-semibold text-gray-900"),
+        content_tag(:span, " #{text}", class: "text-gray-800"),
+        content_tag(:span, " #{destination}", class: "font-semibold text-blue-500")
+      ]
+    end
   end
 end
