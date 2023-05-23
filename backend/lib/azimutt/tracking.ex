bd52e2507515b53f1579a9ef4b1d64e03dd17dc4
Samir Bouaked <sbouaked@pm.me>
2023-05-23 16:38:40 +0200

    extract action to Tracking module

--- backend/lib/azimutt/tracking.ex
+++ backend/lib/azimutt/tracking.ex
@@ -297,4 +297,134 @@ defmodule Azimutt.Tracking do
       type: event.type
     }
   end
+
+  def event_to_action(%{name: "editor_layout_created", created_by: user, project: project}) do
+    %{
+      author: user.name,
+      text: "created a new layout on",
+      destination: project.name
+    }
+  end
+
+  def event_to_action(%{name: "editor_layout_deleted", created_by: user, project: project}) do
+    %{
+      author: user.name,
+      text: "deleted a layout on",
+      destination: project.name
+    }
+  end
+
+  def event_to_action(%{name: "editor_memo_created", created_by: user, project: project}) do
+    %{
+      author: user.name,
+      text: "created a new memo on",
+      destination: project.name
+    }
+  end
+
+  def event_to_action(%{name: "editor_memo_deleted", created_by: user, project: project}) do
+    %{
+      author: user.name,
+      text: "deleted a memo on",
+      destination: project.name
+    }
+  end
+
+  def event_to_action(%{name: "editor_memo_updated", created_by: user, project: project}) do
+    %{
+      author: user.name,
+      text: "updated a new memo on ",
+      destination: project.name
+    }
+  end
+
+  def event_to_action(%{name: "editor_notes_created", created_by: user, project: project}) do
+    %{
+      author: user.name,
+      text: "created a new note on",
+      destination: project.name
+    }
+  end
+
+  def event_to_action(%{name: "editor_notes_deleted", created_by: user, project: project}) do
+    %{
+      author: user.name,
+      text: "deleted a note on",
+      destination: project.name
+    }
+  end
+
+  def event_to_action(%{name: "editor_notes_updated", created_by: user, project: project}) do
+    %{
+      author: user.name,
+      text: "updated a note on",
+      destination: project.name
+    }
+  end
+
+  def event_to_action(%{name: "editor_source_added", created_by: user, project: project}) do
+    %{
+      author: user.name,
+      text: "added a source on",
+      destination: project.name
+    }
+  end
+
+  def event_to_action(%{name: "editor_source_deleted", created_by: user, project: project}) do
+    %{
+      author: user.name,
+      text: "deleted a source on",
+      destination: project.name
+    }
+  end
+
+  def event_to_action(%{name: "editor_source_refreshed", created_by: user, project: project}) do
+    %{
+      author: user.name,
+      text: "refreshed a source on",
+      destination: project.name
+    }
+  end
+
+  def event_to_action(%{name: "project_created", created_by: user, project: project}) do
+    %{
+      author: user.name,
+      text: "created a new project named",
+      destination: project.name
+    }
+  end
+
+  def event_to_action(%{name: "project_deleted", created_by: user, project: project}) do
+    %{
+      author: user.name,
+      text: "deleted",
+      destination: project.name
+    }
+  end
+
+  def event_to_action(%{name: "project_loaded", created_by: user, project: project}) do
+    %{
+      author: user.name,
+      text: "has consulted",
+      destination: project.name
+    }
+  end
+
+  def event_to_action(%{name: "project_updated", created_by: user, project: project}) do
+    %{
+      author: user.name,
+      text: "updated",
+      destination: project.name
+    }
+  end
+
+  ### Here to handle unknown events and prevent the view to cash.
+  ### This is a temporary solution until we have a better way to handle events
+  def event_to_action(created_by: user, project: project) do
+    %{
+      author: user.name,
+      text: "have done something on",
+      destination: project.name
+    }
+  end
 end
--- backend/lib/azimutt_web/views/organization_view.ex
+++ backend/lib/azimutt_web/views/organization_view.ex
@@ -6,133 +6,15 @@ defmodule AzimuttWeb.OrganizationView do
     relative_str
   end
 
-  def event_description(%{name: "editor_layout_created", created_by: user, project: project}) do
-    %{
-      author: user.name,
-      action: "created a new layout on",
-      destination: project.name
-    }
-  end
+  def generate_html_event_description(event) do
+    %{text: text, author: author, destination: destination} = Azimutt.Tracking.event_to_action(event)
 
-  def event_description(%{name: "editor_layout_deleted", created_by: user, project: project}) do
-    %{
-      author: user.name,
-      action: "deleted a layout on",
-      destination: project.name
-    }
-  end
-
-  def event_description(%{name: "editor_memo_created", created_by: user, project: project}) do
-    %{
-      author: user.name,
-      action: "created a new memo on",
-      destination: project.name
-    }
-  end
-
-  def event_description(%{name: "editor_memo_deleted", created_by: user, project: project}) do
-    %{
-      author: user.name,
-      action: "deleted a memo on",
-      destination: project.name
-    }
-  end
-
-  def event_description(%{name: "editor_memo_updated", created_by: user, project: project}) do
-    %{
-      author: user.name,
-      action: "updated a new memo on ",
-      destination: project.name
-    }
-  end
-
-  def event_description(%{name: "editor_notes_created", created_by: user, project: project}) do
-    %{
-      author: user.name,
-      action: "created a new note on",
-      destination: project.name
-    }
-  end
-
-  def event_description(%{name: "editor_notes_deleted", created_by: user, project: project}) do
-    %{
-      author: user.name,
-      action: "deleted a note on",
-      destination: project.name
-    }
-  end
-
-  def event_description(%{name: "editor_notes_updated", created_by: user, project: project}) do
-    %{
-      author: user.name,
-      action: "updated a note on",
-      destination: project.name
-    }
-  end
-
-  def event_description(%{name: "editor_source_added", created_by: user, project: project}) do
-    %{
-      author: user.name,
-      action: "added a source on",
-      destination: project.name
-    }
-  end
-
-  def event_description(%{name: "editor_source_deleted", created_by: user, project: project}) do
-    %{
-      author: user.name,
-      action: "deleted a source on",
-      destination: project.name
-    }
-  end
-
-  def event_description(%{name: "editor_source_refreshed", created_by: user, project: project}) do
-    %{
-      author: user.name,
-      action: "refreshed a source on",
-      destination: project.name
-    }
-  end
-
-  def event_description(%{name: "project_created", created_by: user, project: project}) do
-    %{
-      author: user.name,
-      action: "created a new project named",
-      destination: project.name
-    }
-  end
-
-  def event_description(%{name: "project_deleted", created_by: user, project: project}) do
-    %{
-      author: user.name,
-      action: "deleted",
-      destination: project.name
-    }
-  end
-
-  def event_description(%{name: "project_loaded", created_by: user, project: project}) do
-    %{
-      author: user.name,
-      action: "has consulted",
-      destination: project.name
-    }
-  end
-
-  def event_description(%{name: "project_updated", created_by: user, project: project}) do
-    %{
-      author: user.name,
-      action: "updated",
-      destination: project.name
-    }
-  end
-
-  ### here to handle unknown events and prevent the view to cash.
-  ### This is a temporary solution until we have a better way to handle events
-  def event_description(created_by: user, project: project) do
-    %{
-      author: user.name,
-      action: "have done something on",
-      destination: project.name
-    }
+    raw("""
+    <div class="text-xs">
+      <span class="font-semibold text-gray-900 "> #{author}</span>
+      <span class="text-gray-800"> #{text}</span>
+      <span class="font-semibold text-blue-500"> #{destination}</span>
+    </div>
+    """)
   end
 end
