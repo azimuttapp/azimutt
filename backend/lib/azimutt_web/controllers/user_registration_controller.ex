1d236e8032ce5d527eff73f4cd5d8a0e15d02c18
Loïc Knuchel <loicknuchel@gmail.com>
2024-02-28 21:14:17 +0100

    Block account creation

--- backend/lib/azimutt_web/controllers/user_registration_controller.ex
+++ backend/lib/azimutt_web/controllers/user_registration_controller.ex
@@ -13,28 +13,32 @@ defmodule AzimuttWeb.UserRegistrationController do
   end
 
   def create(conn, %{"user" => user_params}) do
-    now = DateTime.utc_now()
-    auth_method = "password"
-    attrs = user_params |> Mapx.atomize() |> Map.put(:avatar, Faker.Avatar.image_url())
+    conn
+    |> put_flash(:info, "Account creation temporary disabled")
+    |> redirect(to: Routes.user_session_path(conn, :new))
 
-    case Accounts.register_password_user(attrs, UserAuth.get_attribution(conn), now) do
-      {:ok, user} ->
-        Accounts.send_email_confirmation(user, &Routes.user_confirmation_url(conn, :confirm, &1))
-
-        conn
-        |> put_flash(
-          :info,
-          if Azimutt.config(:require_email_confirmation) && !Azimutt.config(:skip_email_confirmation) do
-            "Azimutt account created, please check your emails to validate it and then access Azimutt."
-          else
-            "Welcome to Azimutt 🥳"
-          end
-        )
-        |> UserAuth.login_user(user, auth_method)
-        |> redirect(to: Routes.user_dashboard_path(conn, :index))
-
-      {:error, %Ecto.Changeset{} = changeset} ->
-        conn |> render("new.html", changeset: changeset)
-    end
+    #    now = DateTime.utc_now()
+    #    auth_method = "password"
+    #    attrs = user_params |> Mapx.atomize() |> Map.put(:avatar, Faker.Avatar.image_url())
+    #
+    #    case Accounts.register_password_user(attrs, UserAuth.get_attribution(conn), now) do
+    #      {:ok, user} ->
+    #        Accounts.send_email_confirmation(user, &Routes.user_confirmation_url(conn, :confirm, &1))
+    #
+    #        conn
+    #        |> put_flash(
+    #          :info,
+    #          if Azimutt.config(:require_email_confirmation) && !Azimutt.config(:skip_email_confirmation) do
+    #            "Azimutt account created, please check your emails to validate it and then access Azimutt."
+    #          else
+    #            "Welcome to Azimutt 🥳"
+    #          end
+    #        )
+    #        |> UserAuth.login_user(user, auth_method)
+    #        |> redirect(to: Routes.user_dashboard_path(conn, :index))
+    #
+    #      {:error, %Ecto.Changeset{} = changeset} ->
+    #        conn |> render("new.html", changeset: changeset)
+    #    end
   end
 end
