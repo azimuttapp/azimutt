621cf6ee31fd9350290636b67cb7fa4b54722489
Loïc Knuchel <loicknuchel@gmail.com>
2024-03-27 23:43:45 +0100

    Parse BigQuery url

--- libs/database-types/src/url.ts
+++ libs/database-types/src/url.ts
@@ -4,20 +4,31 @@ import {filterValues} from "@azimutt/utils";
 export type DatabaseUrl = string
 export const DatabaseUrl = z.string()
 export type DatabaseUrlParsed = { full: string, kind?: DatabaseKind, user?: string, pass?: string, host?: string, port?: number, db?: string, options?: string }
-export type DatabaseKind = 'couchbase' | 'mariadb' | 'mongodb' | 'mysql' | 'oracle' | 'postgres' | 'snowflake' | 'sqlite' | 'sqlserver'
+export type DatabaseKind = 'bigquery' | 'couchbase' | 'mariadb' | 'mongodb' | 'mysql' | 'oracle' | 'postgres' | 'snowflake' | 'sqlite' | 'sqlserver'
 
 // vertically align regexes with variable names ^^
-const couchbaseRegexpLonger = /^couchbases?:\/\/(?:([^:]+):([^@]*)@)?([^:/?]+)(?::(\d+))?(?:\/([^?]+))?(?:\?(.+))?$/
-const mariadbRegexpLo = /^(?:jdbc:)?mariadb:\/\/(?:([^:]+):([^@]*)@)?([^:/?]+)(?::(\d+))?(?:\/([^?]+))?(?:\?(.+))?$/
-const mongoRegexLonger = /mongodb(?:\+srv)?:\/\/(?:([^:]+):([^@]*)@)?([^:/?]+)(?::(\d+))?(?:\/([^?]+))?(?:\?(.+))?$/
-const mysqlRegexpLonger = /^(?:jdbc:)?mysql:\/\/(?:([^:]+):([^@]*)@)?([^:/?]+)(?::(\d+))?(?:\/([^?]+))?(?:\?(.+))?$/
-const postres = /^(?:jdbc:)?postgres(?:ql)?:\/\/(?:([^:]+):([^@]*)@)?([^:/?]+)(?::(\d+))?(?:\/([^?]+))?(?:\?(.+))?$/
-const sqlser = /^(?:jdbc:)?sqlserver(?:ql)?:\/\/(?:([^:]+):([^@]*)@)?([^:/?]+)(?::(\d+))?(?:\/([^?]+))?(?:\?(.+))?$/
-const snowflakeRege = /^(?:jdbc:)?snowflake:\/\/(?:([^:]+):([^@]*)@)?([^:/?]+)(?::(\d+))?(?:\/([^?]+))?(?:\?(.+))?$/
-const snowflakeRegexLongerrrrrrrr = /^https:\/\/(?:([^:]+):([^@]*)@)?(.+?(?:\.privatelink)?\.snowflakecomputing\.com)(?::(\d+))?(?:\/([^?]+))?$/
+const bq = /^(?:jdbc:)?bigquery:\/\/(?:([^:]+):([^@]*)@)?(?:https:\/\/)?([^:/?&]+)?(?::(\d+))?(?:\/([^?]+))?(?:\?(.+))?$/
+const couchbaseRegexxxxxxxxxxx = /^couchbases?:\/\/(?:([^:]+):([^@]*)@)?([^:/?&]+)(?::(\d+))?(?:\/([^?]+))?(?:\?(.+))?$/
+const mariadbRegexxxxxxx = /^(?:jdbc:)?mariadb:\/\/(?:([^:]+):([^@]*)@)?([^:/?&]+)(?::(\d+))?(?:\/([^?]+))?(?:\?(.+))?$/
+const mongoRegexxxxxxxxxx = /mongodb(?:\+srv)?:\/\/(?:([^:]+):([^@]*)@)?([^:/?&]+)(?::(\d+))?(?:\/([^?]+))?(?:\?(.+))?$/
+const mysqlRegexxxxxxxxxxx = /^(?:jdbc:)?mysql:\/\/(?:([^:]+):([^@]*)@)?([^:/?&]+)(?::(\d+))?(?:\/([^?]+))?(?:\?(.+))?$/
+const postgresRe = /^(?:jdbc:)?postgres(?:ql)?:\/\/(?:([^:]+):([^@]*)@)?([^:/?&]+)(?::(\d+))?(?:\/([^?]+))?(?:\?(.+))?$/
+const sqlserver = /^(?:jdbc:)?sqlserver(?:ql)?:\/\/(?:([^:]+):([^@]*)@)?([^:/?&]+)(?::(\d+))?(?:\/([^?]+))?(?:\?(.+))?$/
+const snowflakeRegexxx = /^(?:jdbc:)?snowflake:\/\/(?:([^:]+):([^@]*)@)?([^:/?&]+)(?::(\d+))?(?:\/([^?]+))?(?:\?(.+))?$/
+const snowflakeRegexxxxxxxxxxxxxx = /^https:\/\/(?:([^:]+):([^@]*)@)?(.+?(?:\.privatelink)?\.snowflakecomputing\.com)(?::(\d+))?(?:\/([^?]+))?$/
 
 export function parseDatabaseUrl(url: DatabaseUrl): DatabaseUrlParsed {
-    const couchbaseMatches = url.match(couchbaseRegexpLonger)
+    const bigqueryMatches = url.match(bq)
+    if (bigqueryMatches) {
+        const kind: DatabaseKind = 'bigquery'
+        const [, user, pass, host, port, db, optionsStr] = bigqueryMatches
+        const {email: user2, key: pass2, project: db2, ...opts} = Object.fromEntries((optionsStr || '').split('&').map(part => part.split('=')))
+        const options = Object.entries(opts).filter(([k, _]) => k).map(([k, v]) => `${k}=${v}`).join('&') || undefined
+        const values = {kind, user: user || user2, pass: pass || pass2, host, port: port ? parseInt(port) : undefined, db: db || db2, options}
+        return {...filterValues(values, v => v !== undefined), full: url}
+    }
+
+    const couchbaseMatches = url.match(couchbaseRegexxxxxxxxxxx)
     if (couchbaseMatches) {
         const kind: DatabaseKind = 'couchbase'
         const [, user, pass, host, port, db, options] = couchbaseMatches
@@ -25,7 +36,7 @@ export function parseDatabaseUrl(url: DatabaseUrl): DatabaseUrlParsed {
         return {...filterValues(opts, v => v !== undefined), full: url}
     }
 
-    const mariadbMatches = url.match(mariadbRegexpLo)
+    const mariadbMatches = url.match(mariadbRegexxxxxxx)
     if (mariadbMatches) {
         const kind: DatabaseKind = 'mariadb'
         const [, user, pass, host, port, db, options] = mariadbMatches
@@ -33,7 +44,7 @@ export function parseDatabaseUrl(url: DatabaseUrl): DatabaseUrlParsed {
         return {...filterValues(opts, v => v !== undefined), full: url}
     }
 
-    const mongodbMatches = url.match(mongoRegexLonger)
+    const mongodbMatches = url.match(mongoRegexxxxxxxxxx)
     if (mongodbMatches) {
         const kind: DatabaseKind = 'mongodb'
         const [, user, pass, host, port, db, options] = mongodbMatches
@@ -41,7 +52,7 @@ export function parseDatabaseUrl(url: DatabaseUrl): DatabaseUrlParsed {
         return {...filterValues(opts, v => v !== undefined), full: url}
     }
 
-    const mysqlMatches = url.match(mysqlRegexpLonger)
+    const mysqlMatches = url.match(mysqlRegexxxxxxxxxxx)
     if (mysqlMatches) {
         const kind: DatabaseKind = 'mysql'
         const [, user, pass, host, port, db, options] = mysqlMatches
@@ -49,7 +60,7 @@ export function parseDatabaseUrl(url: DatabaseUrl): DatabaseUrlParsed {
         return {...filterValues(opts, v => v !== undefined), full: url}
     }
 
-    const postgresMatches = url.match(postres)
+    const postgresMatches = url.match(postgresRe)
     if (postgresMatches) {
         const kind: DatabaseKind = 'postgres'
         const [, user, pass, host, port, db, options] = postgresMatches
@@ -57,7 +68,7 @@ export function parseDatabaseUrl(url: DatabaseUrl): DatabaseUrlParsed {
         return {...filterValues(opts, v => v !== undefined), full: url}
     }
 
-    const snowflakeMatches = url.match(snowflakeRege) || url.match(snowflakeRegexLongerrrrrrrr)
+    const snowflakeMatches = url.match(snowflakeRegexxx) || url.match(snowflakeRegexxxxxxxxxxxxxx)
     if (snowflakeMatches) {
         const kind: DatabaseKind = 'snowflake'
         const [, user, pass, host, port, db, optionsStr] = snowflakeMatches
@@ -67,7 +78,7 @@ export function parseDatabaseUrl(url: DatabaseUrl): DatabaseUrlParsed {
         return {...filterValues(values, v => v !== undefined), full: url}
     }
 
-    const sqlserverMatches = url.match(sqlser) || parseSqlServerUrl(url)
+    const sqlserverMatches = url.match(sqlserver) || parseSqlServerUrl(url)
     if (sqlserverMatches) {
         const kind: DatabaseKind = 'sqlserver'
         const [, user, pass, host, port, db, options] = sqlserverMatches
