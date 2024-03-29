3d8dda4b4f25f4e3a431ae9e5e83064b37bf747f
Loïc Knuchel <loicknuchel@gmail.com>
2024-03-29 21:31:55 +0100

    Fetch indexes, comments and more stats

--- libs/connector-bigquery/src/bigquery.ts
+++ libs/connector-bigquery/src/bigquery.ts
@@ -2,6 +2,7 @@ import {BigQueryTimestamp, Dataset, DatasetsResponse} from "@google-cloud/bigque
 import {groupBy, Logger, removeUndefined, zip} from "@azimutt/utils"
 import {
     AzimuttColumn,
+    AzimuttIndex,
     AzimuttRelation,
     AzimuttSchema,
     AzimuttTable,
@@ -19,14 +20,13 @@ export const getSchema = (opts: BigquerySchemaOpts) => async (conn: Conn): Promi
     const datasetSchemas = await Promise.all(datasetIds.map(async datasetId => {
         const tables = await getTables(projectId, datasetId, opts)(conn)
         const columns = await getColumns(projectId, datasetId, opts)(conn).then(cols => groupBy(cols, toTableId))
-        const primaryKeys = await getPrimaryKeys(projectId, datasetId, opts)(conn).then(cols => groupBy(cols, toTableId))
+        const primaryKeys = await getPrimaryKeys(projectId, datasetId, opts)(conn).then(pks => groupBy(pks, toTableId))
         const foreignKeys = await getForeignKeys(projectId, datasetId, opts)(conn)
-        // TODO indexes
-        // TODO comments
+        const indexes = await getIndexes(projectId, datasetId, opts)(conn).then(idxs => groupBy(idxs, toTableId))
         return {
             tables: tables.map(table => {
                 const id = toTableId(table)
-                return buildTable(table, columns[id] || [], primaryKeys[id] || [])
+                return buildTable(table, columns[id] || [], primaryKeys[id] || [], indexes[id] || [])
             }),
             relations: foreignKeys.flatMap(buildRelation)
         }
@@ -52,24 +52,50 @@ export type RawTable = {
     table_type: 'BASE TABLE' | 'VIEW' | 'MATERIALIZED VIEW' | 'EXTERNAL' | 'CLONE' | 'SNAPSHOT'
     ddl: string
     creation_time: BigQueryTimestamp
+    view_definition: string | null
+    last_refresh_time: BigQueryTimestamp | null
+    description: string | null
+    labels: string | null // ex: [STRUCT("key1", "value1"), STRUCT("key2", "value2")]
+    partitions: string[]
+    rows: number | null
+    bytes: number | null
+    last_modified: BigQueryTimestamp | null
 }
 
 export const getTables = (projectId: string, datasetId: string, opts: BigquerySchemaOpts) => async (conn: Conn): Promise<RawTable[]> => {
     // https://cloud.google.com/bigquery/docs/information-schema-tables
+    // https://cloud.google.com/bigquery/docs/information-schema-partitions
+    // https://cloud.google.com/bigquery/docs/information-schema-views
+    // https://cloud.google.com/bigquery/docs/information-schema-materialized-views
+    // https://cloud.google.com/bigquery/docs/information-schema-table-options
     return conn.query<RawTable>(`
-        SELECT table_catalog
-             , table_schema
-             , table_name
-             , table_type
-             , ddl
-             , creation_time
-        FROM ${projectId}.${datasetId}.INFORMATION_SCHEMA.TABLES
-        WHERE table_type IN ('BASE TABLE', 'VIEW', 'MATERIALIZED VIEW')${scopeFilter(' AND ', 'table_catalog', opts.catalog, 'table_schema', opts.schema, 'table_name', opts.entity)}
-        ORDER BY table_catalog, table_schema, table_name;`, [], 'getTables'
+        SELECT t.table_catalog
+             , t.table_schema
+             , t.table_name
+             , t.table_type
+             , t.ddl
+             , t.creation_time
+             , v.view_definition
+             , m.last_refresh_time
+             , d.option_value                         AS description
+             , l.option_value                         AS labels
+             , ARRAY_AGG(p.partition_id IGNORE NULLS) AS partitions
+             , SUM(p.total_rows) AS \`rows\`
+             , SUM(p.total_billable_bytes)            AS bytes
+             , MAX(p.last_modified_time)              AS last_modified
+        FROM ${projectId}.${datasetId}.INFORMATION_SCHEMA.TABLES t
+                 LEFT JOIN ${projectId}.${datasetId}.INFORMATION_SCHEMA.PARTITIONS p ON p.table_catalog = t.table_catalog AND p.table_schema = t.table_schema AND p.table_name = t.table_name
+                 LEFT JOIN ${projectId}.${datasetId}.INFORMATION_SCHEMA.VIEWS v ON v.table_catalog = t.table_catalog AND v.table_schema = t.table_schema AND v.table_name = t.table_name
+                 LEFT JOIN ${projectId}.${datasetId}.INFORMATION_SCHEMA.MATERIALIZED_VIEWS m ON m.table_catalog = t.table_catalog AND m.table_schema = t.table_schema AND m.table_name = t.table_name
+                 LEFT JOIN ${projectId}.${datasetId}.INFORMATION_SCHEMA.TABLE_OPTIONS d ON d.table_catalog = t.table_catalog AND d.table_schema = t.table_schema AND d.table_name = t.table_name AND d.option_name = 'description'
+                 LEFT JOIN ${projectId}.${datasetId}.INFORMATION_SCHEMA.TABLE_OPTIONS l ON l.table_catalog = t.table_catalog AND l.table_schema = t.table_schema AND l.table_name = t.table_name AND l.option_name = 'labels'
+        WHERE t.table_type IN ('BASE TABLE', 'VIEW', 'MATERIALIZED VIEW')${scopeFilter(' AND ', 't.table_catalog', opts.catalog, 't.table_schema', opts.schema, 't.table_name', opts.entity)}
+        GROUP BY t.table_catalog, t.table_schema, t.table_name, t.table_type, t.ddl, t.creation_time, v.view_definition, m.last_refresh_time, d.option_value, l.option_value
+        ORDER BY t.table_catalog, t.table_schema, t.table_name;`, [], 'getTables'
     ).catch(handleError(`Failed to get tables for ${projectId}.${datasetId}`, [], opts))
 }
 
-function buildTable(table: RawTable, columns: RawColumn[], primaryKeys: RawPrimaryKey[]): AzimuttTable {
+function buildTable(table: RawTable, columns: RawColumn[], primaryKeys: RawPrimaryKey[], indexes: RawIndex[]): AzimuttTable {
     const pk = primaryKeys[0]
     return removeUndefined({
         // catalog: table.table_catalog,
@@ -79,9 +105,9 @@ function buildTable(table: RawTable, columns: RawColumn[], primaryKeys: RawPrima
         view: table.table_type === 'VIEW' || table.table_type === 'MATERIALIZED VIEW' ? true : undefined,
         primaryKey: pk ? buildPrimaryKey(pk) : undefined,
         uniques: undefined,
-        indexes: undefined,
+        indexes: indexes.length > 0 ? indexes.map(buildIndex) : undefined,
         checks: undefined,
-        comment: undefined
+        comment: table.description ? removeQuotes(table.description) : undefined
     })
 }
 
@@ -95,23 +121,27 @@ export type RawColumn = {
     column_default: string
     column_nullable: boolean
     column_partitioning: boolean
+    description: string | null
 }
 
 export const getColumns = (projectId: string, datasetId: string, opts: BigquerySchemaOpts) => async (conn: Conn): Promise<RawColumn[]> => {
     // https://cloud.google.com/bigquery/docs/information-schema-columns
+    // https://cloud.google.com/bigquery/docs/information-schema-column-field-paths
     return conn.query<RawColumn>(`
-        SELECT table_catalog
-             , table_schema
-             , table_name
-             , ordinal_position AS column_index
-             , column_name
-             , data_type AS column_type
-             , column_default AS column_default
-             , is_nullable = 'YES' AS column_nullable
-             , is_partitioning_column = 'YES' AS column_partitioning
-        FROM ${projectId}.${datasetId}.INFORMATION_SCHEMA.COLUMNS
-        WHERE ordinal_position IS NOT NULL${scopeFilter(' AND ', 'table_catalog', opts.catalog, 'table_schema', opts.schema, 'table_name', opts.entity)}
-        ORDER BY table_catalog, table_schema, table_name, column_index;`, [], 'getColumns'
+        SELECT c.table_catalog
+             , c.table_schema
+             , c.table_name
+             , c.ordinal_position               AS column_index
+             , c.column_name
+             , c.data_type                      AS column_type
+             , c.column_default                 AS column_default
+             , c.is_nullable = 'YES'            AS column_nullable
+             , c.is_partitioning_column = 'YES' AS column_partitioning
+             , p.description
+        FROM ${projectId}.${datasetId}.INFORMATION_SCHEMA.COLUMNS c
+                 JOIN ${projectId}.${datasetId}.INFORMATION_SCHEMA.COLUMN_FIELD_PATHS p ON p.table_catalog = c.table_catalog AND p.table_schema = c.table_schema AND p.table_name = c.table_name AND p.column_name = c.column_name
+        WHERE c.ordinal_position IS NOT NULL${scopeFilter(' AND ', 'c.table_catalog', opts.catalog, 'c.table_schema', opts.schema, 'c.table_name', opts.entity)}
+        ORDER BY c.table_catalog, c.table_schema, c.table_name, c.ordinal_position;`, [], 'getColumns'
     ).catch(handleError(`Failed to get columns for ${projectId}.${datasetId}`, [], opts))
 }
 
@@ -120,8 +150,8 @@ function buildColumn(column: RawColumn): AzimuttColumn {
         name: column.column_name,
         type: column.column_type,
         nullable: column.column_nullable || undefined,
-        default: column.column_default,
-        comment: undefined,
+        default: column.column_default !== 'NULL' ? column.column_default : undefined,
+        comment: column.description || undefined,
         values: undefined,
         columns: undefined
     })
@@ -212,6 +242,52 @@ function buildRelation(fk: RawForeignKey): AzimuttRelation[] {
     })
 }
 
+export type RawIndex = {
+    table_catalog: string
+    table_schema: string
+    table_name: string
+    index_name: string
+    index_columns: string[]
+    index_status: string
+    creation_time: BigQueryTimestamp
+    last_modification_time: BigQueryTimestamp
+    last_refresh_time: BigQueryTimestamp | null
+    disable_reason: string | null
+    ddl: string
+    total_storage_bytes: number
+}
+
+export const getIndexes = (projectId: string, datasetId: string, opts: BigquerySchemaOpts) => async (conn: Conn): Promise<RawIndex[]> => {
+    // https://cloud.google.com/bigquery/docs/information-schema-indexes
+    // https://cloud.google.com/bigquery/docs/information-schema-index-columns
+    return conn.query<RawIndex>(`
+        SELECT i.index_catalog               AS table_catalog
+             , i.index_schema                AS table_schema
+             , i.table_name
+             , i.index_name
+             , ARRAY_AGG(c.index_field_path) AS index_columns
+             , i.index_status
+             , i.creation_time
+             , i.last_modification_time
+             , i.last_refresh_time
+             , i.disable_reason
+             , i.ddl
+             , i.total_storage_bytes
+        FROM ${projectId}.${datasetId}.INFORMATION_SCHEMA.SEARCH_INDEXES i
+                 JOIN ${projectId}.${datasetId}.INFORMATION_SCHEMA.SEARCH_INDEX_COLUMNS c ON c.index_catalog = i.index_catalog AND c.index_schema = i.index_schema AND c.table_name = i.table_name AND c.index_name = i.index_name
+        ${scopeFilter('WHERE ', 'i.index_catalog', opts.catalog, 'i.index_schema', opts.schema, 'i.table_name', opts.entity)}
+        GROUP BY i.index_catalog, i.index_schema, i.table_name, i.index_name, i.index_status, i.creation_time, i.last_modification_time, i.last_refresh_time, i.disable_reason, i.ddl, i.total_storage_bytes;`, [], 'getIndexes'
+    ).catch(handleError(`Failed to get indexes for ${projectId}.${datasetId}`, [], opts))
+}
+
+function buildIndex(index: RawIndex): AzimuttIndex {
+    return {
+        name: index.index_name,
+        columns: index.index_columns,
+        definition: index.ddl
+    }
+}
+
 function handleError<T>(msg: string, onError: T, {logger, ignoreErrors}: BigquerySchemaOpts) {
     return (err: any): Promise<T> => {
         if (ignoreErrors) {
@@ -240,4 +316,10 @@ function datasetFilter(datasetId: string, schema: SchemaName | undefined): boole
     if (schema === datasetId) return true
     if (schema.indexOf('%') && new RegExp(schema.replaceAll('%', '.*')).exec(datasetId)) return true
     return false
+}
+
+function removeQuotes(value: string): string {
+    if (value.startsWith('"') && value.endsWith('"')) return value.slice(1, -1)
+    if (value.startsWith("'") && value.endsWith("'")) return value.slice(1, -1)
+    return value
 }
--- libs/connector-bigquery/tests/bigquery.test.ts
+++ libs/connector-bigquery/tests/bigquery.test.ts
@@ -1,45 +1,41 @@
 import {describe, expect, test} from "@jest/globals";
 import {DatabaseUrlParsed, parseDatabaseUrl} from "@azimutt/database-types";
 import {connect} from "../src/connect";
-import {BigquerySchemaOpts, getForeignKeys, getPrimaryKeys, getSchema, getTables} from "../src/bigquery";
+import {BigquerySchemaOpts, getForeignKeys, getIndexes, getPrimaryKeys, getSchema, getTables} from "../src/bigquery";
 import {application, logger} from "./constants";
 import {getColumns} from "../out/bigquery";
 
 describe.skip('bigquery', () => {
     // fake url, use a real one to test (see README for how-to)
-    const url: DatabaseUrlParsed = parseDatabaseUrl('bigquery://bigquery.googleapis.com/azimutt-experiments?key=local/key.json')
+    const project = 'azimutt-experiments'
+    const dataset = 'relational'
+    const table = 'azimutt_%'
+    const url: DatabaseUrlParsed = parseDatabaseUrl(`bigquery://bigquery.googleapis.com/${project}?key=local/key.json`)
+    const opts: BigquerySchemaOpts = {logger, catalog: project, schema: dataset, entity: table, sampleSize: 10, inferRelations: true, ignoreErrors: false}
+
     test('getSchema', async () => {
-        const opts: BigquerySchemaOpts = {logger, catalog: undefined, schema: 'relational', entity: 'azimutt_%', sampleSize: 10, inferRelations: true, ignoreErrors: false}
         const schema = await connect(application, url, getSchema(opts), {logger, logQueries: true})
         console.log('schema', schema)
         expect(schema.tables.length).toEqual(5)
     }, 15 * 1000)
     test('getTables', async () => {
-        const projectId = 'azimutt-experiments'
-        const datasetId = 'azimutt_connector_trial'
-        const opts: BigquerySchemaOpts = {logger, catalog: undefined, schema: undefined, entity: undefined, sampleSize: 10, inferRelations: true, ignoreErrors: false}
-        const tables = await connect(application, url, getTables(projectId, datasetId, opts), {logger, logQueries: true})
+        const tables = await connect(application, url, getTables(project, dataset, opts), {logger, logQueries: true})
         console.log(`${tables.length} tables`, tables)
     })
     test('getColumns', async () => {
-        const projectId = 'azimutt-experiments'
-        const datasetId = 'azimutt_connector_trial'
-        const opts: BigquerySchemaOpts = {logger, catalog: undefined, schema: undefined, entity: undefined, sampleSize: 10, inferRelations: true, ignoreErrors: false}
-        const columns = await connect(application, url, getColumns(projectId, datasetId, opts), {logger, logQueries: true})
+        const columns = await connect(application, url, getColumns(project, dataset, opts), {logger, logQueries: true})
         console.log(`${columns.length} columns`, columns)
     })
     test('getPrimaryKeys', async () => {
-        const projectId = 'azimutt-experiments'
-        const datasetId = 'relational'
-        const opts: BigquerySchemaOpts = {logger, catalog: undefined, schema: undefined, entity: undefined, sampleSize: 10, inferRelations: true, ignoreErrors: false}
-        const primaryKeys = await connect(application, url, getPrimaryKeys(projectId, datasetId, opts), {logger, logQueries: true})
+        const primaryKeys = await connect(application, url, getPrimaryKeys(project, dataset, opts), {logger, logQueries: true})
         console.log(`${primaryKeys.length} primary keys`, primaryKeys)
     })
     test('getForeignKeys', async () => {
-        const projectId = 'azimutt-experiments'
-        const datasetId = 'relational'
-        const opts: BigquerySchemaOpts = {logger, catalog: projectId, schema: datasetId, entity: 'azimutt_%', sampleSize: 10, inferRelations: true, ignoreErrors: false}
-        const foreignKeys = await connect(application, url, getForeignKeys(projectId, datasetId, opts), {logger, logQueries: true})
+        const foreignKeys = await connect(application, url, getForeignKeys(project, dataset, opts), {logger, logQueries: true})
         console.log(`${foreignKeys.length} foreign keys`, foreignKeys)
+    })
+    test('getIndexes', async () => {
+        const indexes = await connect(application, url, getIndexes(project, dataset, opts), {logger, logQueries: true})
+        console.log(`${indexes.length} indexes`, indexes)
     })
 })
