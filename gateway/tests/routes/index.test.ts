e5bae3f68974dc62c8bcf3cb838a6460c2d98058
Loïc Knuchel <loicknuchel@gmail.com>
2024-04-22 09:40:31 +0200

    fix2

--- gateway/tests/routes/index.test.ts
+++ gateway/tests/routes/index.test.ts
@@ -1,10 +1,16 @@
 import {describe, expect, test} from "vitest"
 import {version} from "../../src/version";
-import {startServer} from "../../src";
-import {configFromEnv} from "../../src/plugins/config";
+import {NodeEnv, startServer} from "../../src";
+import {buildConfig} from "../../src/plugins/config";
 
 describe('GET /', async () => {
-    const server = await startServer(configFromEnv())
+    const server = await startServer(buildConfig({
+        NODE_ENV: NodeEnv.development,
+        LOG_LEVEL: 'info',
+        API_HOST: 'localhost',
+        API_PORT: '3000',
+        CORS_ALLOW_ORIGIN: 'http://localhost:4000',
+    }))
     test('Should return hello world', async () => {
         const response = await server.inject({method: 'GET', path: '/'})
         expect(response.statusCode).eq(200)
