5098f52a19c591a98b1345d6629c0c6d1a07dd60
Anthony Ly <anthonyly.dev@gmail.com>
2024-06-18 17:55:16 +0200

    ui: report sidebar

--- cli/html-report/src/components/report/ReportSidebar/ReportSidebar.tsx
+++ cli/html-report/src/components/report/ReportSidebar/ReportSidebar.tsx
@@ -1,5 +1,28 @@
+import { Badge } from "@/components/ui/badge"
+import { ReportContext } from "@/context/ReportContext"
+import { cn } from "@/lib/utils"
+import { useContext } from "react"
+
 export interface ReportSidebarProps {}
 
 export const ReportSidebar = ({}: ReportSidebarProps) => {
-  return <div>custom</div>
+  const report = useContext(ReportContext)
+  return (
+    <div className="px-1">
+      <ul>
+        {report.map(({ level, levelViolationsCount }: any) => (
+          <li
+            key={level}
+            className={cn(
+              { "opacity-30": levelViolationsCount === 0 },
+              "flex space-y-1"
+            )}
+          >
+            <p className="grow">{level}</p>
+            <Badge>{levelViolationsCount}</Badge>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
 }
