6808af580121014b40b4ca4c25919931b68f1561
Anthony Ly <anthonyly.dev@gmail.com>
2024-06-18 17:55:00 +0200

    chore: report context

--- cli/html-report/src/App.tsx
+++ cli/html-report/src/App.tsx
@@ -1,18 +1,16 @@
-import { useEffect } from "react"
 import { REPORT } from "./constants/report.constants"
 import { Button } from "./components/ui/button"
 import { MainLayout } from "./components/layout/MainLayout"
 import { ReportSidebar } from "./components/report/ReportSidebar/ReportSidebar"
+import { ReportContext } from "./context/ReportContext"
 
 function App() {
-  useEffect(() => {
-    console.log(REPORT)
-  }, [])
-
   return (
-    <MainLayout sidebar={<ReportSidebar />}>
-      <Button>Azimutt</Button>
-    </MainLayout>
+    <ReportContext.Provider value={REPORT}>
+      <MainLayout sidebar={<ReportSidebar />}>
+        <Button>Azimutt</Button>
+      </MainLayout>
+    </ReportContext.Provider>
   )
 }
 
---
+++ cli/html-report/src/context/ReportContext.tsx
@@ -0,0 +1,4 @@
+import { REPORT } from "@/constants/report.constants"
+import { createContext } from "react"
+
+export const ReportContext = createContext<any[]>(REPORT)
