fe036b625a84b9139f5839c9f53c7b50af047b4c
Anthony Ly <anthonyly.dev@gmail.com>
2024-06-20 17:11:16 +0200

    chore: revert gateway from main

--- gateway/src/services/tracking.ts
+++ gateway/src/services/tracking.ts
@@ -1,4 +1,3 @@
-import fetch from "node-fetch";
 import crypto from "node:crypto";
 
 export async function track(name: string, details: object, instance: string, environment: string = 'prod'): Promise<void> {
