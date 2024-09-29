9205b14ab82447a04f867f65a7980afdff4f2a29
Loïc Knuchel <loicknuchel@gmail.com>
2024-09-29 22:30:22 +0200

    publish gateway & cli

--- cli/src/version.ts
+++ cli/src/version.ts
@@ -1,1 +1,1 @@
-export const version = '0.1.25' // FIXME: `process.env.npm_package_version` is not available :/
+export const version = '0.1.26' // FIXME: `process.env.npm_package_version` is not available :/
