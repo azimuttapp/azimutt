11f300e4311f93d34ff3fe6ba1ffdb9ef8954edc
Loïc Knuchel <loicknuchel@gmail.com>
2024-09-29 21:23:18 +0200

    fix cli

--- libs/parser-sql/jest.config.cjs
+++ libs/parser-sql/jest.config.cjs
@@ -1,5 +1,5 @@
 module.exports = {
-    transform: {'^.+\\.ts?$': 'ts-jest'},
+    transform: {'^.+\\.ts$': 'ts-jest'},
     testEnvironment: 'node',
     testRegex: '/src/.+\\.test?\\.(ts|tsx)$',
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
